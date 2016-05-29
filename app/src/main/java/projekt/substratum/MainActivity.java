127fd568fcf186dc8500cd21ff2f93883625d355
Nicholas Chum <nicholaschum@gmail.com>
2016-05-29 18:12:45 -0400

    MainActivity: Allow for packages to be picked up with Layers' Meta

--- app/src/main/java/projekt/substratum/MainActivity.java
+++ app/src/main/java/projekt/substratum/MainActivity.java
@@ -1,24 +1,62 @@
 package projekt.substratum;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public RecyclerView list;
+    public HashMap<String, String[]> layers_packages;
     private String[] headerNamesArray, headerPreviewsArray;
+    private Context mContext;
+
+    public void getLayersPackages(Context context, String package_name) {
+        // Simulate the Layers Plugin feature by filtering all installed apps and their metadata
+        try {
+            ApplicationInfo appInfo = context.getPackageManager().getApplicationInfo(
+                    package_name, PackageManager.GET_META_DATA);
+            if (appInfo.metaData != null) {
+                if (appInfo.metaData.getString("Layers_Name") != null) {
+                    if (appInfo.metaData.getString("Layers_Developer") != null) {
+                        String[] data = {appInfo.metaData.getString("Layers_Developer"),
+                                package_name};
+                        layers_packages.put(appInfo.metaData.getString("Layers_Name"), data);
+                    }
+                }
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
 
+        mContext = this;
+        layers_packages = new HashMap<String, String[]>();
+
         // TODO: Create it so it uses a recyclerView to parse substratum-based themes
 
+        PackageManager packageManager = getPackageManager();
+        List<ApplicationInfo> list = packageManager.getInstalledApplications(PackageManager
+                .GET_META_DATA);
+        for (ApplicationInfo packageInfo : list) {
+            Log.d("Processing package", packageInfo.packageName);
+            getLayersPackages(mContext, packageInfo.packageName);
+        }
+        Log.e("Substratum Ready Themes", Integer.toString(layers_packages.size()));
 
         // Test
 

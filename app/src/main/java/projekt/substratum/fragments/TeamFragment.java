22ef44f98fea91ef6beb42a1ca1e04deadf689ca
Nicholas Chum <nicholaschum@gmail.com>
2016-07-01 01:26:54 -0400

    Add @USA-RedDragon to the contributor list

--- app/src/main/java/projekt/substratum/fragments/TeamFragment.java
+++ app/src/main/java/projekt/substratum/fragments/TeamFragment.java
@@ -62,6 +62,16 @@ public class TeamFragment extends Fragment {
             }
         });
 
+        CardView jacob = (CardView) root.findViewById(R.id.jacob);
+        jacob.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String playURL = getString(R.string.team_jacob_link);
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(playURL));
+                startActivity(i);
+            }
+        });
+
         CardView branden = (CardView) root.findViewById(R.id.branden);
         branden.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {

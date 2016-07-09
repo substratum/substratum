86b3ec9825ff93570dba540cf2fda77b06d44a68
Nicholas Chum <nicholaschum@gmail.com>
2016-07-09 01:26:10 -0400

    AppIntro: Upgrade to the Welcome library by @stephentuso

--- app/src/main/java/projekt/substratum/AppIntroActivity.java
+++ app/src/main/java/projekt/substratum/AppIntroActivity.java
@@ -6,10 +6,9 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
-import java.util.ArrayList;
+import com.stephentuso.welcome.WelcomeScreenHelper;
 
-import za.co.riggaroo.materialhelptutorial.TutorialItem;
-import za.co.riggaroo.materialhelptutorial.tutorial.MaterialTutorialActivity;
+import projekt.substratum.util.AppIntro;
 
 /**
  * @author Nicholas Chum (nicholaschum)
@@ -17,8 +16,8 @@ import za.co.riggaroo.materialhelptutorial.tutorial.MaterialTutorialActivity;
 
 public class AppIntroActivity extends AppCompatActivity {
 
-    private static final int REQUEST_CODE = 1234;
     private SharedPreferences prefs;
+    private WelcomeScreenHelper welcomeScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +27,7 @@ public class AppIntroActivity extends AppCompatActivity {
                 getApplicationContext());
 
         if (prefs.getBoolean("first_run", true)) {
-            loadAppIntro();
+            loadAppIntro(savedInstanceState);
         } else {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
@@ -36,63 +35,31 @@ public class AppIntroActivity extends AppCompatActivity {
         }
     }
 
-    public void loadAppIntro() {
-        Intent mainAct = new Intent(this, MaterialTutorialActivity.class);
-        mainAct.putParcelableArrayListExtra(MaterialTutorialActivity
-                .MATERIAL_TUTORIAL_ARG_TUTORIAL_ITEMS, getTutorialItems());
-        startActivityForResult(mainAct, REQUEST_CODE);
-    }
-
-    private ArrayList<TutorialItem> getTutorialItems() {
-        TutorialItem tutorialItem1 = new TutorialItem(R.string.slide_1_title, R
-                .string.slide_1_text,
-                R.color.slide_1, R.drawable.appintro_slide_1, R.drawable.appintro_slide_1_anim);
-
-        TutorialItem tutorialItem2 = new TutorialItem(R.string.slide_2_title, R
-                .string.slide_2_text,
-                R.color.slide_2, R.drawable.appintro_slide_2, R.drawable.appintro_slide_2);
-
-        TutorialItem tutorialItem3 = new TutorialItem(R.string.slide_3_title, R
-                .string.slide_3_text,
-                R.color.slide_3, R.drawable.appintro_slide_3, R.drawable.appintro_slide_3);
-
-        TutorialItem tutorialItem4 = new TutorialItem(R.string.slide_4_title, R
-                .string.slide_4_text,
-                R.color.slide_4, R.drawable.appintro_slide_4, R.drawable.appintro_slide_4);
-
-        TutorialItem tutorialItem5 = new TutorialItem(R.string.slide_5_title, R
-                .string.slide_5_text,
-                R.color.slide_5, R.drawable.appintro_slide_5, R.drawable.appintro_slide_5);
-
-        TutorialItem tutorialItem6 = new TutorialItem(R.string.slide_6_title, R
-                .string.slide_6_text,
-                R.color.slide_6, R.drawable.appintro_slide_6, R.drawable.appintro_slide_6);
-
-        ArrayList<TutorialItem> tutorialItems = new ArrayList<>();
-        tutorialItems.add(tutorialItem1);
-        tutorialItems.add(tutorialItem2);
-        tutorialItems.add(tutorialItem3);
-        tutorialItems.add(tutorialItem4);
-        tutorialItems.add(tutorialItem5);
-        tutorialItems.add(tutorialItem6);
-
-        return tutorialItems;
+    public void loadAppIntro(Bundle savedInstanceState) {
+        welcomeScreen = new WelcomeScreenHelper(this, AppIntro.class);
+        welcomeScreen.show(savedInstanceState);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == WelcomeScreenHelper.DEFAULT_WELCOME_SCREEN_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                prefs.edit().putBoolean("enable_swapping_overlays", false).apply();
+                prefs.edit().putBoolean("first_run", false).apply();
 
-            // Add all predefined booleans here for the app to function
-            prefs.edit().putBoolean("enable_swapping_overlays", false).apply();
-            prefs.edit().putBoolean("first_run", false).apply();
-
-            // Finally start the MainActivity
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            this.finish();
-        } else {
-            this.finish();
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                this.finish();
+            } else {
+                this.finish();
+            }
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        welcomeScreen.onSaveInstanceState(outState);
     }
 }
---
+++ app/src/main/java/projekt/substratum/util/AppIntro.java
@@ -0,0 +1,37 @@
+package projekt.substratum.util;
+
+import com.stephentuso.welcome.WelcomeScreenBuilder;
+import com.stephentuso.welcome.ui.WelcomeActivity;
+import com.stephentuso.welcome.util.WelcomeScreenConfiguration;
+
+import projekt.substratum.R;
+
+/**
+ * @author Nicholas Chum (nicholaschum)
+ */
+
+public class AppIntro extends WelcomeActivity {
+
+    @Override
+    protected WelcomeScreenConfiguration configuration() {
+        return new WelcomeScreenBuilder(this)
+                .theme(R.style.CustomWelcomeScreenTheme)
+                .defaultTitleTypefacePath("Montserrat-Bold.ttf")
+                .defaultHeaderTypefacePath("Montserrat-Bold.ttf")
+                .basicPage(R.drawable.appintro_slide_1, getString(R.string.slide_1_title),
+                        getString(R.string.slide_1_text), R.color.slide_1)
+                .basicPage(R.drawable.appintro_slide_2, getString(R.string.slide_2_title),
+                        getString(R.string.slide_2_text), R.color.slide_2)
+                .basicPage(R.drawable.appintro_slide_3, getString(R.string.slide_3_title),
+                        getString(R.string.slide_3_text), R.color.slide_3)
+                .basicPage(R.drawable.appintro_slide_4, getString(R.string.slide_4_title),
+                        getString(R.string.slide_4_text), R.color.slide_4)
+                .basicPage(R.drawable.appintro_slide_5, getString(R.string.slide_5_title),
+                        getString(R.string.slide_5_text), R.color.slide_5)
+                .basicPage(R.drawable.appintro_slide_6, getString(R.string.slide_6_title),
+                        getString(R.string.slide_6_text), R.color.slide_6)
+                .swipeToDismiss(true)
+                .exitAnimation(android.R.anim.fade_out)
+                .build();
+    }
+}

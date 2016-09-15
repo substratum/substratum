86971a2e11b43a01480c18d6c31f6ddeafb1da03
codekidX <ckrackerx15@gmail.com>
2016-09-15 20:25:03 +0530

    TroubleshootFragment: incorporate dynamic list and save resources

--- app/src/main/java/projekt/substratum/fragments/TroubleshootingFragment.java
+++ app/src/main/java/projekt/substratum/fragments/TroubleshootingFragment.java
@@ -5,8 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import projekt.substratum.R;
+import projekt.substratum.adapters.TroubleShootAdapter;
 
 /**
  * @author Nicholas Chum (nicholaschum)yea
@@ -14,10 +16,45 @@ import projekt.substratum.R;
 
 public class TroubleshootingFragment extends Fragment {
 
+    ListView troubleshootListView;
+
+    int[] troubleshootQuestions = {R.string.question_one,
+                               R.string.question_two,
+                               R.string.question_three,
+                               R.string.question_four,
+                               R.string.question_five,
+                               R.string.question_six,
+                               R.string.question_seven,
+                               R.string.question_eight,
+                               R.string.question_nine,
+                               R.string.question_ten,
+                               R.string.question_eleven,
+                               R.string.question_twelve };
+
+    int[] troubleshootAnswers = {R.string.answer_one,
+                                 R.string.answer_two,
+                                 R.string.answer_three,
+                                 R.string.answer_four,
+                                 R.string.answer_five,
+                                 R.string.answer_six,
+                                 R.string.answer_seven,
+                                 R.string.answer_eight,
+                                 R.string.answer_nine,
+                                 R.string.answer_ten,
+                                 R.string.answer_eleven,
+                                 R.string.answer_twelve};
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
             savedInstanceState) {
         super.onCreate(savedInstanceState);
-        return inflater.inflate(R.layout.troubleshooting_fragment, null);
+        View root = inflater.inflate(R.layout.troubleshooting_fragment, null);
+
+        troubleshootListView = (ListView) root.findViewById(R.id.troubleshoot_list_view);
+
+        // make sure troubleshootQuestions & troubleshootAnswers are of same length and then assign adapter to listView
+        troubleshootListView.setAdapter(new TroubleShootAdapter(troubleshootQuestions,troubleshootAnswers,getActivity().getApplicationContext()));
+
+        return root;
     }
 }

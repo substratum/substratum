d0de6fe662e3e958c753e62ae75329dc9ffee2a5
codekidX <ckrackerx15@gmail.com>
2016-09-15 20:32:44 +0530

    TroubleshootFragment: add adapter and row

---
+++ app/src/main/java/projekt/substratum/adapters/TroubleShootAdapter.java
@@ -0,0 +1,67 @@
+package projekt.substratum.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import projekt.substratum.R;
+
+/**
+ * Created by snowpuppet on 15/09/16.
+ */
+public class TroubleShootAdapter extends BaseAdapter {
+
+    int[] tQues;
+    int[] tAns;
+    Context context;
+
+    // Views
+    TextView questionsTextView;
+    TextView answersTextView;
+
+    public TroubleShootAdapter(int[] tQues, int[] tAns, Context context) {
+        this.tQues = tQues;
+        this.tAns = tAns;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return tQues.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return tQues[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View v = inflater.inflate(R.layout.troubleshoot_row,viewGroup,false);
+
+        questionsTextView = (TextView) v.findViewById(R.id.trouble_ques);
+        answersTextView = (TextView) v.findViewById(R.id.trouble_ans);
+
+        String question = getStringFromResource(tQues[i]);
+        String answer = getStringFromResource(tAns[i]);
+
+        questionsTextView.setText(question);
+        answersTextView.setText(answer);
+
+        return v;
+    }
+
+    private String getStringFromResource(int stringId) {
+        return context.getResources().getString(stringId);
+    }
+}

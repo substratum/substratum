80b5d054a9f327ec98b26b6d0f0dc516fe819540
Nicholas Chum <nicholaschum@gmail.com>
2016-12-21 18:21:03 -0500

    DetectionReceiver: Slimify the algorithm to detect new theme installs

--- app/src/main/java/projekt/substratum/services/DetectionReceiver.java
+++ app/src/main/java/projekt/substratum/services/DetectionReceiver.java
@@ -9,7 +9,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.net.Uri;
+import android.content.pm.ResolveInfo;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
@@ -28,43 +28,17 @@ import projekt.substratum.util.SubstratumThemeUpdater;
 public class DetectionReceiver extends BroadcastReceiver {
 
     private Context mContext;
-    private String new_theme_name;
-    private Boolean new_theme = false;
-    private Boolean new_setup = false;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         this.mContext = context;
-        Uri packageName = intent.getData();
-        String package_name = packageName.toString().substring(8);
-        try {
-            ApplicationInfo appInfo = context.getPackageManager().getApplicationInfo(
-                    package_name, PackageManager.GET_META_DATA);
-            if (appInfo.metaData != null) {
-                if (!References.checkOMS(mContext)) {
-                    if (appInfo.metaData.getBoolean(References.metadataLegacy, false)) {
-                        if (appInfo.metaData.getString(References.metadataName) != null) {
-                            if (appInfo.metaData.getString(References.metadataAuthor) != null) {
-                                Log.d("SubstratumDetector", "Substratum is now initializing: " +
-                                        package_name);
-                                MainFunction mainFunction = new MainFunction();
-                                mainFunction.execute("");
-                            }
-                        }
-                    }
-                } else {
-                    if (appInfo.metaData.getString(References.metadataName) != null) {
-                        if (appInfo.metaData.getString(References.metadataAuthor) != null) {
-                            Log.d("SubstratumDetector", "Substratum is now initializing: " +
-                                    package_name);
-                            MainFunction mainFunction = new MainFunction();
-                            mainFunction.execute("");
-                        }
-                    }
-                }
+        String package_name = intent.getData().toString().substring(8);
+        List<ResolveInfo> themes = References.getThemes(context);
+        for (int i = 0; i < themes.size(); i++) {
+            if (themes.get(i).activityInfo.packageName.equals(package_name)) {
+                MainFunction mainFunction = new MainFunction();
+                mainFunction.execute("");
             }
-        } catch (Exception e) {
-            // Exception
         }
     }
 
@@ -72,14 +46,7 @@ public class DetectionReceiver extends BroadcastReceiver {
 
         @Override
         protected void onPostExecute(String result) {
-            // Everything below will only run as long as the PackageManager changes
-            if (new_theme && !new_setup) {
-                new SubstratumThemeUpdater().initialize(mContext, new_theme_name, false);
-                new_theme = false;
-            } else {
-                new_theme = false;
-                new_setup = false;
-            }
+            new SubstratumThemeUpdater().initialize(mContext, result, false);
             super.onPostExecute(result);
         }
 
@@ -96,75 +63,55 @@ public class DetectionReceiver extends BroadcastReceiver {
                     ApplicationInfo appInfo = mContext.getPackageManager().getApplicationInfo(
                             packageInfo.packageName, PackageManager.GET_META_DATA);
                     if (appInfo.metaData != null) {
-                        if (References.checkOMS(mContext)) {
-                            if (appInfo.metaData.getString(References.metadataName) != null) {
-                                if (appInfo.metaData.getString(References.metadataAuthor) != null) {
+                        if (appInfo.metaData.getString(References.metadataName) != null) {
+                            if (appInfo.metaData.getString(References.metadataAuthor) != null) {
+                                if (References.checkOMS(mContext)) {
                                     installed.add(packageInfo.packageName);
-                                }
-                            }
-                        } else {
-                            if (appInfo.metaData.getString(References.metadataName) != null) {
-                                if (appInfo.metaData.getString(References.metadataAuthor) != null) {
-                                    if (appInfo.metaData.getBoolean(References.metadataLegacy,
-                                            false)) {
-                                        installed.add(packageInfo.packageName);
-                                    } else {
-                                        Log.e("SubstratumCacher", "Device is non-OMS, while an " +
-                                                "OMS theme is installed, aborting operation!");
+                                } else if (appInfo.metaData.getBoolean(
+                                        References.metadataLegacy, false)) {
+                                    installed.add(packageInfo.packageName);
+                                } else {
+                                    Log.e("SubstratumCacher", "Device is non-OMS, while an " +
+                                            "OMS theme is installed, aborting operation!");
 
-                                        Intent showIntent = new Intent();
-                                        PendingIntent contentIntent = PendingIntent.getActivity(
-                                                mContext, 0, showIntent, 0);
+                                    Intent showIntent = new Intent();
+                                    PendingIntent contentIntent = PendingIntent.getActivity(
+                                            mContext, 0, showIntent, 0);
 
-                                        String parse = String.format(mContext.getString(
-                                                R.string.failed_to_install_text_notification),
-                                                appInfo.metaData.getString(References
-                                                        .metadataName));
+                                    String parse = String.format(mContext.getString(
+                                            R.string.failed_to_install_text_notification),
+                                            appInfo.metaData.getString(
+                                                    References.metadataName));
 
-                                        NotificationManager notificationManager =
-                                                (NotificationManager) mContext.getSystemService(
-                                                        Context.NOTIFICATION_SERVICE);
-                                        NotificationCompat.Builder mBuilder =
-                                                new NotificationCompat.Builder(mContext)
-                                                        .setContentIntent(contentIntent)
-                                                        .setAutoCancel(true)
-                                                        .setSmallIcon(
-                                                                R.drawable
-                                                                        .notification_warning_icon)
-                                                        .setContentTitle(mContext.getString(
-                                                                R.string.failed_to_install_title_notification))
-                                                        .setContentText(parse);
-                                        Notification notification = mBuilder.build();
-                                        notificationManager.notify(
-                                                References.notification_id, notification);
+                                    NotificationManager notificationManager =
+                                            (NotificationManager) mContext.getSystemService(
+                                                    Context.NOTIFICATION_SERVICE);
+                                    NotificationCompat.Builder mBuilder =
+                                            new NotificationCompat.Builder(mContext)
+                                                    .setContentIntent(contentIntent)
+                                                    .setAutoCancel(true)
+                                                    .setSmallIcon(
+                                                            R.drawable.notification_warning_icon)
+                                                    .setContentTitle(mContext.getString(
+                                                            R.string.failed_to_install_title_notification))
+                                                    .setContentText(parse);
+                                    Notification notification = mBuilder.build();
+                                    notificationManager.notify(
+                                            References.notification_id, notification);
 
-                                        String final_commands = "pm uninstall " +
-                                                packageInfo.packageName;
-
-                                        if (References.isPackageInstalled(mContext,
-                                                "masquerade.substratum")) {
-                                            Intent runCommand = new Intent();
-                                            runCommand.addFlags(Intent
-                                                    .FLAG_INCLUDE_STOPPED_PACKAGES);
-                                            runCommand.setAction("masquerade.substratum.COMMANDS");
-                                            runCommand.putExtra("om-commands", final_commands);
-                                            mContext.sendBroadcast(runCommand);
-                                        } else {
-                                            References.runCommands(final_commands);
-                                        }
-                                    }
+                                    String final_commands = "pm uninstall " +
+                                            packageInfo.packageName;
+                                    References.runCommands(final_commands);
                                 }
                             }
                         }
                     }
                 } catch (Exception e) {
-                    Log.e(References.SUBSTRATUM_LOG, "Unable to find package identifier (INDEX " +
-                            "OUT OF " +
-                            "BOUNDS)");
+                    Log.e(References.SUBSTRATUM_LOG,
+                            "The Substratum theme detection algorithm has encountered an error.");
                 }
             }
             // Check for SharedPreferences Set and sort it
-            if (!prefs.contains("installed_themes")) new_setup = true;
             Set<String> setString = prefs.getStringSet("installed_themes", new HashSet<>());
             Set<String> setStringSorted = new TreeSet<>();
             setStringSorted.addAll(setString);
@@ -181,12 +128,9 @@ public class DetectionReceiver extends BroadcastReceiver {
                     .equals(setStringSorted)) {
                 for (int i = 0; i < installed_setStringSorted.size(); i++) {
                     if (!setStringSorted.contains(installed.get(i))) {
-                        new_theme = true;
-                        new_theme_name = installed.get(i);
                         i = installed_setStringSorted.size();
-                        SharedPreferences.Editor edit = prefs.edit();
-                        edit.putStringSet("installed_themes", installed_set);
-                        edit.apply();
+                        prefs.edit().putStringSet("installed_themes", installed_set).apply();
+                        return installed.get(i);
                     }
                 }
             }

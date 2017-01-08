1d127c291f7d9ddb0d635f962aa6a93e6fd7b5f7
Jack Boswell (boswelja) <boswelja@outlook.com>
2017-01-09 11:57:07 +1300

    Fix some more warnings

--- app/src/main/java/projekt/substratum/fragments/TimePickerFragment.java
+++ app/src/main/java/projekt/substratum/fragments/TimePickerFragment.java
@@ -3,6 +3,7 @@ package projekt.substratum.fragments;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.text.format.DateFormat;
 import android.widget.Button;
@@ -24,6 +25,7 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
         flag = f;
     }
 
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final Calendar c = Calendar.getInstance();

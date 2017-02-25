c0be57876faa75dfef87860219e2ef33ddf37c06
Nathan Chancellor <natechancellor@gmail.com>
2017-02-25 02:23:19 -0700

    ElevatedCommands: Add some comments
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- app/src/main/java/projekt/substratum/config/ElevatedCommands.java
+++ app/src/main/java/projekt/substratum/config/ElevatedCommands.java
@@ -8,18 +8,22 @@ import static projekt.substratum.config.References.MASQUERADE_PACKAGE;
 
 public class ElevatedCommands {
 
+    // Run the specified commands in a root shell
     public static void runCommands(final String commands) {
         Root.runCommand(commands);
     }
 
+    // Reboot the device
     public static void reboot() {
         Root.runCommand("reboot");
     }
 
+    // Kill the masquerade package to force it to restart
     public static void restartMasquerade() {
         Root.runCommand("pkill -f " + MASQUERADE_PACKAGE);
     }
 
+    // Kill zygote to force a soft rboot
     public static void softReboot() {
         Root.runCommand("pkill -f zygote");
     }

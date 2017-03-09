481ab7900710715f3bc667ca431de20e60223a89
Ivan Iskandar <iiiiskandar14@gmail.com>
2017-03-09 19:35:18 +0700

    Introduce the App Crash receiver
    When broadcast received, it will disable all enabled overlays for
    specified package name.

---
+++ app/src/main/java/projekt/substratum/services/AppCrashReceiver.java
@@ -0,0 +1,50 @@
+package projekt.substratum.services;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import projekt.substratum.R;
+import projekt.substratum.config.References;
+import projekt.substratum.config.ThemeManager;
+
+public class AppCrashReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (!References.checkOMS(context)) {
+            return;
+        }
+
+        String packageName = intent.getStringExtra("projekt.android.EXTRA_PACKAGE_NAME");
+        String exceptionClass = intent.getStringExtra("projekt.android.EXTRA_EXCEPTION_CLASS_NAME");
+        boolean repeating = intent.getBooleanExtra("projekt.android.EXTRA_CRASH_REPEATING", false);
+
+        if ((exceptionClass.contains("NotFoundException") ||
+                exceptionClass.contains("IllegalArgumentException")) && repeating) {
+            List<String> overlays = ThemeManager.listEnabledOverlaysForTarget(packageName);
+
+            if (overlays.size() > 0) {
+                try {
+                    ApplicationInfo applicationInfo = context.getPackageManager()
+                            .getApplicationInfo(packageName, 0);
+                    packageName = context.getPackageManager()
+                            .getApplicationLabel(applicationInfo).toString();
+                } catch (PackageManager.NameNotFoundException e) {
+                    // exception
+                }
+
+                Toast.makeText(context, context.getString(R.string.app_crash_toast, packageName),
+                        Toast.LENGTH_LONG)
+                        .show();
+                ThemeManager.disableOverlay(context, new ArrayList<>(overlays));
+            }
+        }
+    }
+}

423e43603c41b788d7ff150d139dc14514e6a101
Nicholas Chum <nicholaschum@gmail.com>
2017-03-12 08:45:01 -0400

    AppCrashReceiver: Rewrite the receiver to fire a notification
    This also properly checks if it is repeating. Only repeat offences will be disabled.
    
    By detecting EXTRA_CRASH_REPEATING, this is a native Android framework code that reports if the system detects multiple force closes that the user could ignore.

--- app/src/main/java/projekt/substratum/services/AppCrashReceiver.java
+++ app/src/main/java/projekt/substratum/services/AppCrashReceiver.java
@@ -1,11 +1,13 @@
 package projekt.substratum.services;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.widget.Toast;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +16,12 @@ import projekt.substratum.R;
 import projekt.substratum.config.References;
 import projekt.substratum.config.ThemeManager;
 
+import static android.content.Context.NOTIFICATION_SERVICE;
+
 public class AppCrashReceiver extends BroadcastReceiver {
+
+    public final static String APP_CRASH_LOG_TAG = "AppCrashReceiver";
+    public final static int NOTIFICATION_ID = 2476;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -29,8 +36,10 @@ public class AppCrashReceiver extends BroadcastReceiver {
         boolean repeating =
                 intent.getBooleanExtra("projekt.substratum.EXTRA_CRASH_REPEATING", false);
 
-        if ((exceptionClass.contains("NotFoundException") ||
-                exceptionClass.contains("IllegalArgumentException")) && repeating) {
+        if (repeating) {
+            Log.e(APP_CRASH_LOG_TAG, "\'" + packageName + "\' stopped unexpectedly...");
+            Log.e(APP_CRASH_LOG_TAG, "Now disabling all overlays for \'" + packageName + "\'...");
+
             List<String> overlays = ThemeManager.listEnabledOverlaysForTarget(packageName);
 
             if (overlays.size() > 0) {
@@ -40,14 +49,25 @@ public class AppCrashReceiver extends BroadcastReceiver {
                     packageName = context.getPackageManager()
                             .getApplicationLabel(applicationInfo).toString();
                 } catch (PackageManager.NameNotFoundException e) {
-                    // exception
+                    e.printStackTrace();
                 }
+                String app_crash_title =
+                        String.format(context.getString(R.string.app_crash_title), packageName);
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+                builder.setSmallIcon(R.drawable.notification_overlay_corruption);
+                builder.setContentTitle(app_crash_title);
+                builder.setContentText(context.getString(R.string.app_crash_content));
+                builder.setOngoing(true);
+                builder.setPriority(NotificationCompat.PRIORITY_MAX);
+                builder.setCategory(NotificationCompat.CATEGORY_SERVICE);
+                NotificationManager mNotifyMgr =
+                        (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+                mNotifyMgr.notify(NOTIFICATION_ID, builder.build());
 
-                Toast.makeText(context, context.getString(R.string.app_crash_toast, packageName),
-                        Toast.LENGTH_LONG)
-                        .show();
                 ThemeManager.disableOverlay(context, new ArrayList<>(overlays));
             }
+        } else {
+            Log.e(APP_CRASH_LOG_TAG, packageName + " stopped unexpectedly, not repeating...");
         }
     }
 }

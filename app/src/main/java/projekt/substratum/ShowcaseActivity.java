0d5b358588c3de6a9c9a4f2778b75b1cfad0ad2c
Nicholas Chum <nicholaschum@gmail.com>
2017-04-01 18:42:46 -0400

    FileDownloader: Migrate to our centralized code

--- app/src/main/java/projekt/substratum/ShowcaseActivity.java
+++ app/src/main/java/projekt/substratum/ShowcaseActivity.java
@@ -40,17 +40,12 @@ import android.view.View;
 import android.widget.RelativeLayout;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Map;
 
 import projekt.substratum.adapters.ShowcaseTabsAdapter;
 import projekt.substratum.config.References;
+import projekt.substratum.util.FileDownloader;
 import projekt.substratum.util.MD5;
 import projekt.substratum.util.ReadShowcaseTabsFile;
 
@@ -60,13 +55,12 @@ public class ShowcaseActivity extends AppCompatActivity {
     private RelativeLayout no_network;
     private SwipeRefreshLayout swipeRefreshLayout;
     private SharedPreferences prefs;
-    private MenuItem alphabetizeMenu;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.showcase_menu, menu);
 
-        alphabetizeMenu = menu.findItem(R.id.alphabetize);
+        MenuItem alphabetizeMenu = menu.findItem(R.id.alphabetize);
         boolean alphabetize = prefs.getBoolean("alphabetize_showcase", false);
         if (alphabetize) {
             alphabetizeMenu.setIcon(R.drawable.actionbar_alphabetize);
@@ -119,9 +113,7 @@ public class ShowcaseActivity extends AppCompatActivity {
     private void swipeRefresh() {
         swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id
                 .swipeRefreshLayout);
-        swipeRefreshLayout.setOnRefreshListener(() -> {
-            this.recreate();
-        });
+        swipeRefreshLayout.setOnRefreshListener(this::recreate);
     }
 
     private void refreshLayout() {
@@ -275,10 +267,6 @@ public class ShowcaseActivity extends AppCompatActivity {
         protected String doInBackground(String... sUrl) {
             String inputFileName = sUrl[1];
 
-            InputStream input = null;
-            OutputStream output = null;
-            HttpURLConnection connection = null;
-
             File current_wallpapers = new File(getApplicationContext().getCacheDir() +
                     "/ShowcaseCache/" + inputFileName);
             if (current_wallpapers.exists()) {
@@ -287,58 +275,7 @@ public class ShowcaseActivity extends AppCompatActivity {
                         ".xml";
             }
 
-            try {
-                URL url = new URL(sUrl[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-
-                // expect HTTP 200 OK, so we don't mistakenly save error report
-                // instead of the file
-                if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                    return "Server returned HTTP " + connection.getResponseCode()
-                            + " " + connection.getResponseMessage();
-                }
-
-                // this will be useful to display download percentage
-                // might be -1: server did not report the length
-                int fileLength = connection.getContentLength();
-
-                // download the file
-                input = connection.getInputStream();
-
-                output = new FileOutputStream(
-                        getApplicationContext().getCacheDir().getAbsolutePath() +
-                                "/ShowcaseCache/" + inputFileName);
-
-                byte data[] = new byte[4096];
-                long total = 0;
-                int count;
-                while ((count = input.read(data)) != -1) {
-                    // allow canceling with back button
-                    if (isCancelled()) {
-                        input.close();
-                        return null;
-                    }
-                    total += count;
-                    // publishing the progress....
-                    if (fileLength > 0) // only if total length is known
-                        publishProgress((int) (total * 100 / fileLength));
-                    output.write(data, 0, count);
-                }
-            } catch (Exception e) {
-                return e.toString();
-            } finally {
-                try {
-                    if (output != null)
-                        output.close();
-                    if (input != null)
-                        input.close();
-                } catch (IOException ignored) {
-                }
-
-                if (connection != null)
-                    connection.disconnect();
-            }
+            FileDownloader.init(getApplicationContext(), sUrl[0], inputFileName, "ShowcaseCache");
             return inputFileName;
         }
     }
--- app/src/main/java/projekt/substratum/fragments/ShowcaseTab.java
+++ app/src/main/java/projekt/substratum/fragments/ShowcaseTab.java
@@ -31,12 +31,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Map;
@@ -49,6 +43,7 @@ import projekt.substratum.adapters.WallpaperAdapter;
 import projekt.substratum.config.References;
 import projekt.substratum.model.ShowcaseItem;
 import projekt.substratum.model.WallpaperEntries;
+import projekt.substratum.util.FileDownloader;
 import projekt.substratum.util.MD5;
 import projekt.substratum.util.ReadCloudShowcaseFile;
 
@@ -58,7 +53,6 @@ public class ShowcaseTab extends Fragment {
 
     private ViewGroup root;
     private RecyclerView mRecyclerView;
-    private ShowcaseItemAdapter mAdapter;
     private MaterialProgressBar materialProgressBar;
     private View no_network, no_wallpapers;
     private int current_tab_position;
@@ -126,7 +120,7 @@ public class ShowcaseTab extends Fragment {
         protected void onPostExecute(ArrayList result) {
             super.onPostExecute(result);
 
-            mAdapter = new ShowcaseItemAdapter(result);
+            ShowcaseItemAdapter mAdapter = new ShowcaseItemAdapter(result);
             mRecyclerView.setAdapter(mAdapter);
 
             if (result.size() == 0) no_wallpapers.setVisibility(View.VISIBLE);
@@ -139,140 +133,82 @@ public class ShowcaseTab extends Fragment {
         protected ArrayList doInBackground(String... sUrl) {
             String inputFileName = sUrl[1];
             ArrayList<ShowcaseItem> wallpapers = new ArrayList<>();
-            try {
-                InputStream input = null;
-                OutputStream output = null;
-                HttpURLConnection connection = null;
 
-                File showcase_directory = new File(getContext().getCacheDir() +
-                        "/ShowcaseCache/");
-                if (!showcase_directory.exists()) {
-                    Boolean made = showcase_directory.mkdir();
-                    if (!made)
-                        Log.e(References.SUBSTRATUM_LOG, "Could not make showcase directory...");
-                }
+            File showcase_directory = new File(getContext().getCacheDir() +
+                    "/ShowcaseCache/");
+            if (!showcase_directory.exists()) {
+                Boolean made = showcase_directory.mkdir();
+                if (!made)
+                    Log.e(References.SUBSTRATUM_LOG, "Could not make showcase directory...");
+            }
 
-                File current_wallpapers = new File(getContext().getCacheDir() +
-                        "/ShowcaseCache/" + inputFileName);
-                if (current_wallpapers.exists()) {
-                    // We create a temporary file to check whether we should be replacing the
-                    // current
-                    inputFileName = inputFileName.substring(0, inputFileName.length() - 4) +
-                            ".xml";
-                }
+            File current_wallpapers = new File(getContext().getCacheDir() +
+                    "/ShowcaseCache/" + inputFileName);
+            if (current_wallpapers.exists()) {
+                // We create a temporary file to check whether we should be replacing the
+                // current
+                inputFileName = inputFileName.substring(0, inputFileName.length() - 4) +
+                        ".xml";
+            }
 
-                try {
-                    URL url = new URL(sUrl[0]);
-                    connection = (HttpURLConnection) url.openConnection();
-                    connection.connect();
+            FileDownloader.init(getContext(), sUrl[0], inputFileName, "ShowcaseCache");
 
-                    // expect HTTP 200 OK, so we don't mistakenly save error report
-                    // instead of the file
-                    if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                        Log.e("Server returned HTTP", connection.getResponseCode()
-                                + " " + connection.getResponseMessage());
-                    }
-
-                    // this will be useful to display download percentage
-                    // might be -1: server did not report the length
-                    int fileLength = connection.getContentLength();
-
-                    // download the file
-                    input = connection.getInputStream();
-
-                    output = new FileOutputStream(
-                            getContext().getCacheDir().getAbsolutePath() +
-                                    "/ShowcaseCache/" + inputFileName);
-
-                    byte data[] = new byte[4096];
-                    long total = 0;
-                    int count;
-                    while ((count = input.read(data)) != -1) {
-                        // allow canceling with back button
-                        if (isCancelled()) {
-                            input.close();
-                            return null;
-                        }
-                        total += count;
-                        // publishing the progress....
-                        if (fileLength > 0) // only if total length is known
-                            publishProgress((int) (total * 100 / fileLength));
-                        output.write(data, 0, count);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        if (output != null)
-                            output.close();
-                        if (input != null)
-                            input.close();
-                    } catch (IOException ignored) {
-                    }
-
-                    if (connection != null)
-                        connection.disconnect();
-                }
-
-                if (inputFileName.endsWith("-temp.xml")) {
-                    String existing = MD5.calculateMD5(new File(getContext().getCacheDir() +
+            if (inputFileName.endsWith("-temp.xml")) {
+                String existing = MD5.calculateMD5(new File(getContext().getCacheDir() +
+                        "/ShowcaseCache/" + sUrl[1]));
+                String new_file = MD5.calculateMD5(new File(getContext().getCacheDir() +
+                        "/ShowcaseCache/" + inputFileName));
+                if (existing != null && !existing.equals(new_file)) {
+                    Log.e("ShowcaseActivity", "Tab " + current_tab_position +
+                            " has been updated from the cloud!");
+                    File renameMe = new File(getContext().getCacheDir() +
+                            "/ShowcaseCache/" +
+                            sUrl[1].substring(0, sUrl[1].length() - 4) + "-temp.xml");
+                    Boolean renamed = renameMe.renameTo(new File(getContext().getCacheDir() +
                             "/ShowcaseCache/" + sUrl[1]));
-                    String new_file = MD5.calculateMD5(new File(getContext().getCacheDir() +
-                            "/ShowcaseCache/" + inputFileName));
-                    if (existing != null && !existing.equals(new_file)) {
-                        Log.e("ShowcaseActivity", "Tab " + current_tab_position +
-                                " has been updated from the cloud!");
-                        File renameMe = new File(getContext().getCacheDir() +
-                                "/ShowcaseCache/" +
-                                sUrl[1].substring(0, sUrl[1].length() - 4) + "-temp.xml");
-                        Boolean renamed = renameMe.renameTo(new File(getContext().getCacheDir() +
-                                "/ShowcaseCache/" + sUrl[1]));
-                        if (!renamed) Log.e(References.SUBSTRATUM_LOG,
-                                "Could not replace the old tab file with the new tab file...");
-                    } else {
-                        File deleteMe = new File(getContext().getCacheDir() +
-                                "/" + inputFileName);
-                        Boolean deleted = deleteMe.delete();
-                        if (!deleted) Log.e(References.SUBSTRATUM_LOG,
-                                "Could not delete temporary tab file...");
-                    }
+                    if (!renamed) Log.e(References.SUBSTRATUM_LOG,
+                            "Could not replace the old tab file with the new tab file...");
+                } else {
+                    File deleteMe = new File(getContext().getCacheDir() +
+                            "/" + inputFileName);
+                    Boolean deleted = deleteMe.delete();
+                    if (!deleted) Log.e(References.SUBSTRATUM_LOG,
+                            "Could not delete temporary tab file...");
                 }
+            }
 
-                inputFileName = sUrl[1];
+            inputFileName = sUrl[1];
 
-                String[] checkerCommands = {getContext().getCacheDir() +
-                        "/ShowcaseCache/" + inputFileName};
+            String[] checkerCommands = {getContext().getCacheDir() +
+                    "/ShowcaseCache/" + inputFileName};
 
-                @SuppressWarnings("unchecked")
-                final Map<String, String> newArray = ReadCloudShowcaseFile.main(checkerCommands);
-                ShowcaseItem newEntry = new ShowcaseItem();
+            @SuppressWarnings("unchecked")
+            final Map<String, String> newArray = ReadCloudShowcaseFile.main(checkerCommands);
+            ShowcaseItem newEntry = new ShowcaseItem();
 
-                for (String key : newArray.keySet()) {
-                    if (!key.toLowerCase().contains("-".toLowerCase())) {
+            for (String key : newArray.keySet()) {
+                if (!key.toLowerCase().contains("-".toLowerCase())) {
+                    newEntry.setContext(getContext());
+                    newEntry.setThemeName(key);
+                    newEntry.setThemeLink(newArray.get(key));
+                } else {
+                    if (key.toLowerCase().contains("-author".toLowerCase())) {
+                        newEntry.setThemeAuthor(newArray.get(key));
+                    } else if (key.toLowerCase().contains("-pricing".toLowerCase())) {
+                        newEntry.setThemePricing(newArray.get(key));
+                    } else if (key.toLowerCase().contains("-image-override")) {
+                        newEntry.setThemeIcon(newArray.get(key));
+                    } else if (key.toLowerCase().contains("-feature-image")) {
+                        newEntry.setThemeBackgroundImage(newArray.get(key));
+                    } else if (key.toLowerCase().contains("-package-name")) {
+                        newEntry.setThemePackage(newArray.get(key));
+                    } else if (key.toLowerCase().contains("-support".toLowerCase())) {
+                        newEntry.setThemeSupport(newArray.get(key));
+                        wallpapers.add(newEntry);
+                        newEntry = new ShowcaseItem();
                         newEntry.setContext(getContext());
-                        newEntry.setThemeName(key);
-                        newEntry.setThemeLink(newArray.get(key));
-                    } else {
-                        if (key.toLowerCase().contains("-author".toLowerCase())) {
-                            newEntry.setThemeAuthor(newArray.get(key));
-                        } else if (key.toLowerCase().contains("-pricing".toLowerCase())) {
-                            newEntry.setThemePricing(newArray.get(key));
-                        } else if (key.toLowerCase().contains("-image-override")) {
-                            newEntry.setThemeIcon(newArray.get(key));
-                        } else if (key.toLowerCase().contains("-feature-image")) {
-                            newEntry.setThemeBackgroundImage(newArray.get(key));
-                        } else if (key.toLowerCase().contains("-package-name")) {
-                            newEntry.setThemePackage(newArray.get(key));
-                        } else if (key.toLowerCase().contains("-support".toLowerCase())) {
-                            newEntry.setThemeSupport(newArray.get(key));
-                            wallpapers.add(newEntry);
-                            newEntry = new ShowcaseItem();
-                            newEntry.setContext(getContext());
-                        }
                     }
                 }
-            } catch (Exception e) {
-                //
             }
             // Shuffle the deck - every time it will change the order of themes!
             long seed = System.nanoTime();

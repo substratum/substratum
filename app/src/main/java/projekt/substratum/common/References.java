1ca80f252c079adc9a14a5ca1552e037577cf7e6
Ivan Iskandar <iiiiskandar14@gmail.com>
2017-05-02 07:51:57 +0700

    References: Also reset display old themes option

--- app/src/main/java/projekt/substratum/common/References.java
+++ app/src/main/java/projekt/substratum/common/References.java
@@ -546,6 +546,7 @@ public class References {
         editor.putString("compiler", "aapt");
         editor.putBoolean("crash_receiver", true);
         editor.putBoolean("enable_swapping_overlays", false);
+        editor.putBoolean("display_old_themes", false);
 
         // Initial parse of what is installed on the device
         Set<String> installed_themes = new TreeSet<>();

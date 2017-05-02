7fe77b875838889bc83ef0ed4cce0a4baa04fab2
Nicholas Chum <nicholaschum@gmail.com>
2017-05-02 02:18:14 -0400

    ManageSpaceActivity: Finish the activity on configuration clear

--- app/src/main/java/projekt/substratum/activities/launch/ManageSpaceActivity.java
+++ app/src/main/java/projekt/substratum/activities/launch/ManageSpaceActivity.java
@@ -87,6 +87,7 @@ public class ManageSpaceActivity extends AppCompatActivity {
                         References.loadDefaultConfig(this);
                         cacheCounter.setText(
                                 Formatter.formatFileSize(this, getFileSize(getCacheDir())));
+                        finish();
                     })
                     .create();
             dialog.show();

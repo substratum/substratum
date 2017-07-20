52c8c56e19f4ea652603ef8a8736fa92d999f3ca
Harsh Shandilya <msfjarvis@gmail.com>
2017-07-20 18:12:00 +0530

    References: Clear the logchar auto cleanup prefs on reset

--- app/src/main/java/projekt/substratum/common/References.java
+++ app/src/main/java/projekt/substratum/common/References.java
@@ -697,8 +697,10 @@ public class References {
         editor.putBoolean("theme_updater", false);
         editor.putBoolean("show_dangerous_samsung_overlays", false);
         editor.putBoolean("autosave_logchar", true);
+        editor.putBoolean("automatic_logchar_cleanup",false);
         editor.remove("display_old_themes");
         editor.remove("seen_restore_warning");
+        editor.remove("last_logchar_cleanup");
 
         // Initial parse of what is installed on the device
         Set<String> installed_themes = new TreeSet<>();

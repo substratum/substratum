78ab3a4e8dfcda152bc8796a12d13785b95d15f3
Nicholas Chum <nicholaschum@gmail.com>
2017-08-25 07:59:25 -0400

    AndromedaIntegration: Priority switching is instant now

--- app/src/main/java/projekt/substratum/fragments/PriorityListFragment.java
+++ app/src/main/java/projekt/substratum/fragments/PriorityListFragment.java
@@ -179,7 +179,7 @@ public class PriorityListFragment extends Fragment {
                     } catch (Exception e) {
                         // Consume window refresh
                     }
-                }, REFRESH_WINDOW_DELAY * 2);
+                }, (References.checkAndromeda(getContext()) ? 0 : REFRESH_WINDOW_DELAY * 2));
             }
         });
         return root;

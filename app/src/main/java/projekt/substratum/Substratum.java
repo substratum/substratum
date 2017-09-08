a112c7e6f6baed0cc3bfba8c37ba8d813b46578c
Nicholas Chum <nicholaschum@gmail.com>
2017-09-08 05:45:49 -0400

    Revert "Substratum: Change notification to IMPORTANCE_LOW for Andromeda"
    This reverts commit 364393a4e972eafd8a8ac96b11968488182fa172.

--- app/src/main/java/projekt/substratum/Substratum.java
+++ app/src/main/java/projekt/substratum/Substratum.java
@@ -103,7 +103,7 @@ public class Substratum extends Application {
             NotificationChannel andromedaChannel = new NotificationChannel(
                     References.ANDROMEDA_NOTIFICATION_CHANNEL_ID,
                     getString(R.string.notification_channel_andromeda),
-                    NotificationManager.IMPORTANCE_LOW);
+                    NotificationManager.IMPORTANCE_NONE);
             andromedaChannel.setDescription(
                     getString(R.string.notification_channel_andromeda_description));
             notificationManager.createNotificationChannel(andromedaChannel);

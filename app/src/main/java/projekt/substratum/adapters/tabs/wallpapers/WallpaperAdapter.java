a4a0687d67fa895cbd55c7f0e96865d6f4cc30ca
Nicholas Chum <nicholaschum@gmail.com>
2017-09-21 16:22:49 -0400

    WallpaperAdapter: Add a fade_in animation for wallpapers

--- app/src/main/java/projekt/substratum/adapters/tabs/wallpapers/WallpaperAdapter.java
+++ app/src/main/java/projekt/substratum/adapters/tabs/wallpapers/WallpaperAdapter.java
@@ -30,6 +30,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -68,6 +70,11 @@ public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperAdapter.View
         View view = LayoutInflater.from(
                 viewGroup.getContext()).inflate(R.layout.wallpaper_entry_card, viewGroup, false);
         return new ViewHolder(view);
+    }
+
+    private void setAnimation(Context context, View view) {
+        Animation animation = AnimationUtils.loadAnimation(context, android.R.anim.fade_in);
+        view.startAnimation(animation);
     }
 
     @Override
@@ -193,6 +200,7 @@ public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperAdapter.View
             });
             builder.show();
         });
+        setAnimation(mContext, viewHolder.itemView);
     }
 
     @Override

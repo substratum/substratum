f3c171e297441cee71a9e65ddbd6e0ba021b14be
Jack Boswell (boswelja) <boswelja@outlook.com>
2017-10-13 16:26:46 +1300

    Clean up a bit

--- app/src/main/java/projekt/substratum/InformationActivity.java
+++ app/src/main/java/projekt/substratum/InformationActivity.java
@@ -429,7 +429,7 @@ public class InformationActivity extends SubstratumActivity {
         tabLayout = findViewById(R.id.tabs);
         if (tabLayout != null) {
             // First, take account for whether the theme was launched normally
-            if (theme_mode.equals("")) {
+            if (theme_mode.isEmpty()) {
                 try {
                     Context otherContext = mContext.createPackageContext
                             (theme_pid, 0);

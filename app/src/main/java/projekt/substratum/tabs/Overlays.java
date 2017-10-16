substratum/substratum

commit 3b73609d5f8d3c22ca50341bbfcb76df37b5979e

Overlays: Remove redundant handling for icon pack overlays
Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

app/src/main/java/projekt/substratum/tabs/Overlays.java
@@ -728,18 +728,10 @@ public class Overlays extends Fragment {
     }
 
     protected List<String> updateEnabledOverlays() {
-        List<String> state5 = ThemeManager.listOverlays(getContext(), ThemeManager.STATE_ENABLED);
-        ArrayList<String> all = new ArrayList<>(state5);
-
-        all_installed_overlays = new ArrayList<>();
-
-        // ValidatorFilter out icon pack overlays from all overlays
-        for (int i = 0; i < all.size(); i++) {
-            if (!all.get(i).endsWith(".icon")) {
-                all_installed_overlays.add(all.get(i));
-            }
-        }
-        return new ArrayList<>(all_installed_overlays);
+        return new ArrayList<>(ThemeManager.listOverlays(
+                getContext(),
+                ThemeManager.STATE_DISABLED
+        ));
     }
 
     protected boolean checkActiveNotifications() {

app/src/main/java/projekt/substratum/fragments/ManagerFragment.java
@@ -389,17 +389,7 @@ public class ManagerFragment extends Fragment implements SearchView.OnQueryTextL
     }
 
     private List<String> updateEnabledOverlays() {
-        List<String> state5 = ThemeManager.listOverlays(getContext(), STATE_ENABLED);
-        ArrayList<String> all = new ArrayList<>(state5);
-        ArrayList<String> all_installed_overlays = new ArrayList<>();
-
-        // Filter out icon pack overlays from all overlays
-        for (int i = 0; i < all.size(); i++) {
-            if (!all.get(i).endsWith(".icon")) {
-                all_installed_overlays.add(all.get(i));
-            }
-        }
-        return new ArrayList<>(all_installed_overlays);
+        return new ArrayList<>(ThemeManager.listOverlays(getContext(), STATE_ENABLED));
     }
 
     @Override

app/src/main/java/projekt/substratum/fragments/ProfileFragment.java
@@ -933,12 +933,10 @@ public class ProfileFragment extends Fragment {
                             String targetPackage = profile.get(i).get(1);
                             if (Packages.isPackageInstalled(profileFragment.mContext,
                                     targetPackage)) {
-                                if (!packageName.endsWith(".icon")) {
-                                    if (system.contains(packageName)) {
-                                        to_be_run.add(packageName);
-                                    } else {
-                                        profileFragment.cannot_run_overlays.add(profile.get(i));
-                                    }
+                                if (system.contains(packageName)) {
+                                    to_be_run.add(packageName);
+                                } else {
+                                    profileFragment.cannot_run_overlays.add(profile.get(i));
                                 }
                             }
                         }

app/src/main/java/projekt/substratum/services/profiles/ScheduledProfileService.java
@@ -180,12 +180,10 @@ public class ScheduledProfileService extends JobService {
                         String packageName = profile.get(i).get(0);
                         String targetPackage = profile.get(i).get(1);
                         if (Packages.isPackageInstalled(context, targetPackage)) {
-                            if (!packageName.endsWith(".icon")) {
-                                if (system.contains(packageName)) {
-                                    to_be_run.add(packageName);
-                                } else {
-                                    cannot_run_overlays.add(profile.get(i));
-                                }
+                            if (system.contains(packageName)) {
+                                to_be_run.add(packageName);
+                            } else {
+                                cannot_run_overlays.add(profile.get(i));
                             }
                         }
                     }

commit 89fbf57038e7efaac400ad95f01be4052ac20027

Overlays: Fix detection of enabled overlays
* Regression introduced in commit 3b73609d5f8d3c22ca50341bbfcb76df37b5979e

Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

app/src/main/java/projekt/substratum/tabs/Overlays.java
@@ -726,7 +726,7 @@ public class Overlays extends Fragment {
     protected List<String> updateEnabledOverlays() {
         return new ArrayList<>(ThemeManager.listOverlays(
                 getContext(),
-                ThemeManager.STATE_DISABLED
+                ThemeManager.STATE_ENABLED
         ));
     }
 
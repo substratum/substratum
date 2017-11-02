substratum/substratum

commit e322c38ceef6b4dc9403ad5edff4732e49c6f525

Substratum: Use svg animation instead of animation list.
This will also reducing the final apk file size.
Preview: https://photos.app.goo.gl/b9oNGnwi0KAToET72

Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

app/src/main/java/projekt/substratum/activities/launch/SplashScreenActivity.java
@@ -31,6 +31,8 @@ import android.os.Handler;
 import android.util.Log;
 import android.widget.ImageView;
 
+import com.jaredrummler.android.widget.AnimatedSvgView;
+
 import java.io.File;
 import java.lang.ref.WeakReference;
 import java.text.SimpleDateFormat;
@@ -62,8 +64,6 @@ public class SplashScreenActivity extends Activity {
 
     private static final int DELAY_LAUNCH_MAIN_ACTIVITY = 600;
     private static final int DELAY_LAUNCH_APP_INTRO = 2300;
-    @BindView(R.id.splashscreen_image)
-    ImageView splashScreenImage;
     private Intent intent;
 
     @Override
@@ -82,15 +82,9 @@ public class SplashScreenActivity extends Activity {
             // set its background to our AnimationDrawable XML resource.
 
             try {
-                splashScreenImage.setImageDrawable(getDrawable(R.drawable.splashscreen_intro));
 
-                // Get the background, which has been compiled to an AnimationDrawable object.
-                AnimationDrawable frameAnimation =
-                        (AnimationDrawable) splashScreenImage.getDrawable();
-
-                // Start the animation
-                frameAnimation.setOneShot(true);
-                frameAnimation.run();
+                AnimatedSvgView svgView = findViewById(R.id.animated_svg_view);
+                svgView.start();
 
                 // Finally set the proper launch activity and delay
                 intent = new Intent(SplashScreenActivity.this, AppIntroActivity.class);

app/src/main/res/layout/splashscreen_layout.xml
@@ -16,22 +16,27 @@
   ~ You should have received a copy of the GNU General Public License
   ~ along with Substratum.  If not, see <http://www.gnu.org/licenses/>.
   -->
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+              android:layout_width="fill_parent"
+              android:layout_height="fill_parent"
+              xmlns:app="http://schemas.android.com/apk/res-auto"
+              android:layout_gravity="center"
+              android:background="@color/splashscreen_background"
+              android:gravity="center"
+              android:orientation="vertical">
 
-<LinearLayout
-    xmlns:android="http://schemas.android.com/apk/res/android"
-    android:layout_width="fill_parent"
-    android:layout_height="fill_parent"
-    android:layout_gravity="center"
-    android:background="@color/splashscreen_background"
-    android:gravity="center"
-    android:orientation="vertical">
-
-    <ImageView
-        android:id="@+id/splashscreen_image"
-        android:layout_width="@dimen/app_splash_icon_width"
-        android:layout_height="@dimen/app_splash_icon_height"
-        android:contentDescription="@string/app_name"
-        android:src="@drawable/substratum_icon"/>
+    <com.jaredrummler.android.widget.AnimatedSvgView
+        android:id="@+id/animated_svg_view"
+        android:layout_width="128dp"
+        android:layout_height="128dp"
+        android:layout_gravity="center"
+        android:layout_marginBottom="40dp"
+        app:animatedSvgFillColors="@array/substratum_glyph_colors"
+        app:animatedSvgFillStart="1200"
+        app:animatedSvgFillTime="1000"
+        app:animatedSvgGlyphStrings="@array/substratum_glyph_strings"
+        app:animatedSvgTraceTime="2000"
+        app:animatedSvgTraceTimePerGlyph="1000"/>
 
     <ProgressBar
         android:id="@+id/progress_bar_loader"

commit fe747e4d908d6f11819ea833a2f8aaa6ac607444

Substratum: Fix splashscreen layout for subsequent launches
* The new animation from Piere removed the image view
  we used to show the static logo for subsequent launches,
  resulting in the splash screen being a simple white page
  with the loading spinner. Ugly.
* Fix this by bringing back the ImageView and manipulating
  the visibilities of the animated and static logos inside
  SplashScreenActivity.

Fixes: e322c38ceef6b4dc9403ad5edff4732e49c6f525
Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

app/src/main/java/projekt/substratum/activities/launch/SplashScreenActivity.java
@@ -29,6 +29,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.jaredrummler.android.widget.AnimatedSvgView;
@@ -64,6 +65,10 @@ public class SplashScreenActivity extends Activity {
 
     private static final int DELAY_LAUNCH_MAIN_ACTIVITY = 600;
     private static final int DELAY_LAUNCH_APP_INTRO = 2300;
+    @BindView(R.id.animated_svg_view)
+    AnimatedSvgView svgView;
+    @BindView(R.id.splashscreen_image)
+    ImageView splashScreenImage;
     private Intent intent;
 
     @Override
@@ -83,16 +88,18 @@ public class SplashScreenActivity extends Activity {
 
             try {
 
-                AnimatedSvgView svgView = findViewById(R.id.animated_svg_view);
+                splashScreenImage.setVisibility(View.GONE);
                 svgView.start();
 
                 // Finally set the proper launch activity and delay
                 intent = new Intent(SplashScreenActivity.this, AppIntroActivity.class);
                 intent_launch_delay = DELAY_LAUNCH_APP_INTRO;
-            } catch (OutOfMemoryError oome) {
+            } catch (OutOfMemoryError ignored) {
                 Log.e(References.SUBSTRATUM_LOG, "The VM has blown up and the rendering of " +
                         "the splash screen animated icon has been cancelled.");
             }
+        } else {
+            svgView.setVisibility(View.GONE);
         }
 
         Handler handler = new Handler();

app/src/main/res/layout/splashscreen_layout.xml
@@ -25,6 +25,13 @@
               android:gravity="center"
               android:orientation="vertical">
 
+    <ImageView
+        android:id="@+id/splashscreen_image"
+        android:layout_width="@dimen/app_splash_icon_width"
+        android:layout_height="@dimen/app_splash_icon_height"
+        android:contentDescription="@string/app_name"
+        android:src="@drawable/substratum_icon"/>
+
     <com.jaredrummler.android.widget.AnimatedSvgView
         android:id="@+id/animated_svg_view"
         android:layout_width="128dp"
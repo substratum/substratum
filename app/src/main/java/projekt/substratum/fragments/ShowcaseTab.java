substratum/substratum

commit fcadbe40cb44e93efdddb4edde6e53075e646131

SubstratumUI: Add in a brand new default, simple UI
- Default UI for the application from now on, users can revert back to advanced mode in Settings to re-enable Profiles, Priorities, etc.

- No longer allow the user to swipe to refresh on the main themes section. The system will do it automatically when installing/uninstalling any app.

- Showcase is no longer an activity, it has been completely moved to a fragment. This also deprecates the sorting mode for alphabetizing the list. The shortcut has been moved.

- Brand new, full white UI including a white navigation bar for API27! This is easily themable as most, if not all values should be exposed. 

- Settings: when reloading the UI under theming, close the app and reopen it with new UI changes, telling the user in a toast that a restart has been performed

- Default UI will hide team & contributors, licenses, profiles, priorities, community/featured content and the drawer (obviously).

- Default UI uses two new fonts, one for the product title (substratum theme engine) and one for the global UI (Product Sans). This is overlayable by themers INSIDE special UI (default) mode. Not in advanced mode, as that reverts back to default.

app/src/main/java/projekt/substratum/fragments/ShowcaseTab.java
@@ -51,7 +51,6 @@ import projekt.substratum.adapters.tabs.wallpapers.WallpaperAdapter;
 import projekt.substratum.adapters.tabs.wallpapers.WallpaperEntries;
 import projekt.substratum.common.References;
 import projekt.substratum.util.files.FileDownloader;
-import projekt.substratum.util.files.MD5;
 import projekt.substratum.util.readers.ReadCloudShowcaseFile;
 
 import static projekt.substratum.common.References.SHOWCASE_SHUFFLE_COUNT;
@@ -60,10 +59,6 @@ public class ShowcaseTab extends Fragment {
 
     @BindView(R.id.progress_bar_loader)
     ProgressBar materialProgressBar;
-    @BindView(R.id.no_network)
-    View no_network;
-    @BindView(R.id.none_found)
-    View no_wallpapers;
     @BindView(R.id.wallpaperRecyclerView)
     RecyclerView mRecyclerView;
     private int current_tab_position;
@@ -101,20 +96,15 @@ public class ShowcaseTab extends Fragment {
         ArrayList<WallpaperEntries> empty_array = new ArrayList<>();
         RecyclerView.Adapter empty_adapter = new WallpaperAdapter(empty_array);
         mRecyclerView.setAdapter(empty_adapter);
-        no_wallpapers.setVisibility(View.GONE);
-        no_network.setVisibility(View.GONE);
-
         if (References.isNetworkAvailable(mContext)) {
             downloadResources downloadTask = new downloadResources(this);
-            downloadTask.execute(current_tab_address,
+            downloadTask.execute(
+                    current_tab_address,
                     "showcase_tab_" + current_tab_position + ".xml");
         } else {
             mRecyclerView.setVisibility(View.GONE);
             materialProgressBar.setVisibility(View.GONE);
-            no_wallpapers.setVisibility(View.GONE);
-            no_network.setVisibility(View.VISIBLE);
         }
-
     }
 
     /**
@@ -147,9 +137,6 @@ public class ShowcaseTab extends Fragment {
             if (showcaseTab != null) {
                 ShowcaseItemAdapter mAdapter = new ShowcaseItemAdapter(result);
                 showcaseTab.mRecyclerView.setAdapter(mAdapter);
-
-                if (result.isEmpty()) showcaseTab.no_wallpapers.setVisibility(View.VISIBLE);
-
                 showcaseTab.mRecyclerView.setVisibility(View.VISIBLE);
                 showcaseTab.materialProgressBar.setVisibility(View.GONE);
             }
@@ -161,7 +148,6 @@ public class ShowcaseTab extends Fragment {
             ArrayList<ShowcaseItem> wallpapers = new ArrayList<>();
             if (showcaseTab != null) {
                 String inputFileName = sUrl[1];
-
                 File showcase_directory = new File(
                         showcaseTab.mContext.getCacheDir() + "/ShowcaseCache/");
                 if (!showcase_directory.exists()) {
@@ -173,40 +159,10 @@ public class ShowcaseTab extends Fragment {
                 File current_wallpapers = new File(showcaseTab.mContext.getCacheDir() +
                         "/ShowcaseCache/" + inputFileName);
                 if (current_wallpapers.exists()) {
-                    // We create a temporary file to check whether we should be replacing the
-                    // current
                     inputFileName = inputFileName.substring(0, inputFileName.length() - 4) + ".xml";
                 }
 
                 FileDownloader.init(showcaseTab.mContext, sUrl[0], inputFileName, "ShowcaseCache");
-
-                if (inputFileName.endsWith("-temp.xml")) {
-                    String existing = MD5.calculateMD5(new File(showcaseTab.mContext
-                            .getCacheDir() +
-                            "/ShowcaseCache/" + sUrl[1]));
-                    String new_file = MD5.calculateMD5(new File(showcaseTab.mContext
-                            .getCacheDir() +
-                            "/ShowcaseCache/" + inputFileName));
-                    if ((existing != null) && !existing.equals(new_file)) {
-                        Log.e("ShowcaseActivity", "Tab " + showcaseTab.current_tab_position +
-                                " has been updated from the cloud!");
-                        File renameMe = new File(showcaseTab.mContext.getCacheDir() +
-                                "/ShowcaseCache/" +
-                                sUrl[1].substring(0, sUrl[1].length() - 4) + "-temp.xml");
-                        Boolean renamed = renameMe.renameTo(new File(
-                                showcaseTab.mContext.getCacheDir() +
-                                        "/ShowcaseCache/" + sUrl[1]));
-                        if (!renamed) Log.e(References.SUBSTRATUM_LOG,
-                                "Could not replace the old tab file with the new tab file...");
-                    } else {
-                        File deleteMe = new File(showcaseTab.mContext.getCacheDir() +
-                                "/" + inputFileName);
-                        Boolean deleted = deleteMe.delete();
-                        if (!deleted) Log.e(References.SUBSTRATUM_LOG,
-                                "Could not delete temporary tab file...");
-                    }
-                }
-
                 inputFileName = sUrl[1];
 
                 @SuppressWarnings("unchecked") Map<String, String> newArray =
@@ -222,23 +178,18 @@ public class ShowcaseTab extends Fragment {
                         newEntry.setThemeName(stringStringEntry.getKey());
                         newEntry.setThemeLink(stringStringEntry.getValue());
                     } else {
-                        if (stringStringEntry.getKey().toLowerCase(Locale.US)
-                                .contains("-author".toLowerCase(Locale.US))) {
+                        String entry = stringStringEntry.getKey().toLowerCase(Locale.US);
+                        if (entry.contains("-author".toLowerCase(Locale.US))) {
                             newEntry.setThemeAuthor(stringStringEntry.getValue());
-                        } else if (stringStringEntry.getKey().toLowerCase(Locale.US)
-                                .contains("-pricing".toLowerCase(Locale.US))) {
+                        } else if (entry.contains("-pricing".toLowerCase(Locale.US))) {
                             newEntry.setThemePricing(stringStringEntry.getValue());
-                        } else if (stringStringEntry.getKey().toLowerCase(Locale.US)
-                                .contains("-image-override")) {
+                        } else if (entry.contains("-image-override".toLowerCase(Locale.US))) {
                             newEntry.setThemeIcon(stringStringEntry.getValue());
-                        } else if (stringStringEntry.getKey().toLowerCase(Locale.US)
-                                .contains("-feature-image")) {
+                        } else if (entry.contains("-feature-image".toLowerCase(Locale.US))) {
                             newEntry.setThemeBackgroundImage(stringStringEntry.getValue());
-                        } else if (stringStringEntry.getKey().toLowerCase(Locale.US)
-                                .contains("-package-name")) {
+                        } else if (entry.contains("-package-name".toLowerCase(Locale.US))) {
                             newEntry.setThemePackage(stringStringEntry.getValue());
-                        } else if (stringStringEntry.getKey().toLowerCase(Locale.US)
-                                .contains("-support".toLowerCase(Locale.US))) {
+                        } else if (entry.contains("-support".toLowerCase(Locale.US))) {
                             newEntry.setThemeSupport(stringStringEntry.getValue());
                             wallpapers.add(newEntry);
                             newEntry = new ShowcaseItem();
@@ -248,12 +199,8 @@ public class ShowcaseTab extends Fragment {
                 }
                 // Shuffle the deck - every time it will change the order of themes!
                 long seed = System.nanoTime();
-                boolean alphabetize = showcaseTab.prefs.getBoolean("alphabetize_showcase",
-                        false);
-                if (!alphabetize) {
-                    for (int i = 0; i <= SHOWCASE_SHUFFLE_COUNT; i++)
-                        Collections.shuffle(wallpapers, new Random(seed));
-                }
+                for (int i = 0; i <= SHOWCASE_SHUFFLE_COUNT; i++)
+                    Collections.shuffle(wallpapers, new Random(seed));
             }
             return wallpapers;
         }

app/build.gradle
@@ -165,7 +165,9 @@ dependencies {
     // Dependency Injection
     compile 'com.jakewharton:butterknife:8.8.1'
     annotationProcessor 'com.jakewharton:butterknife-compiler:8.8.1'
-    //Splash screen svg animation
+    // Splash screen svg animation
     implementation 'com.jaredrummler:animated-svg-view:1.0.5'
+    // Bottom bar
+    implementation 'com.roughike:bottom-bar:2.3.1'
 }
 apply plugin: 'com.google.gms.google-services'

app/src/main/AndroidManifest.xml
@@ -24,7 +24,9 @@
 
     <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
     <uses-permission android:name="android.permission.ACCESS_SUPERUSER" />
-    <uses-permission android:name="android.permission.CHANGE_CONFIGURATION" />
+    <uses-permission
+        android:name="android.permission.CHANGE_CONFIGURATION"
+        tools:ignore="ProtectedPermissions" />
     <uses-permission
         android:name="android.permission.INSTALL_PACKAGES"
         tools:ignore="ProtectedPermissions" />
@@ -35,16 +37,22 @@
     <uses-permission android:name="android.permission.KILL_BACKGROUND_PROCESSES" />
     <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
     <uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED" />
-    <uses-permission android:name="android.permission.SET_PROCESS_LIMIT" />
+    <uses-permission
+        android:name="android.permission.SET_PROCESS_LIMIT"
+        tools:ignore="ProtectedPermissions" />
     <uses-permission android:name="android.permission.SET_WALLPAPER" />
     <uses-permission
         android:name="android.permission.SET_WALLPAPER_COMPONENT"
         tools:ignore="ProtectedPermissions" />
-    <uses-permission android:name="android.permission.SYSTEM_ALERT_WINDOW" />
+    <uses-permission
+        android:name="android.permission.SYSTEM_ALERT_WINDOW"
+        tools:ignore="ProtectedPermissions" />
     <uses-permission android:name="android.permission.SYSTEM_OVERLAY_WINDOW" />
     <uses-permission android:name="android.permission.VIBRATE" />
     <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
-    <uses-permission android:name="android.permission.WRITE_SETTINGS" />
+    <uses-permission
+        android:name="android.permission.WRITE_SETTINGS"
+        tools:ignore="ProtectedPermissions" />
     <uses-permission android:name="android.permission.WAKE_LOCK" />
     <uses-permission android:name="com.android.launcher.permission.INSTALL_SHORTCUT" />
     <uses-permission android:name="com.samsung.android.permission.MODIFY_THEME" />
@@ -210,14 +218,6 @@
             android:launchMode="singleTask"
             android:parentActivityName=".MainActivity"
             android:theme="@style/AppTheme.NoActionBar">
-            <intent-filter>
-                <action android:name="android.intent.action.MAIN" />
-            </intent-filter>
-        </activity>
-        <activity
-            android:name=".activities.showcase.ShowcaseActivity"
-            android:configChanges="keyboardHidden|screenSize"
-            android:theme="@style/AppTheme">
             <intent-filter>
                 <action android:name="android.intent.action.MAIN" />
             </intent-filter>

app/src/main/java/projekt/substratum/InformationActivity.java
@@ -442,10 +442,14 @@ public class InformationActivity extends AppCompatActivity {
         }
 
         // Themer's booleans to adjust actionbar and navbar colors
-        boolean dynamicActionBarColors = getResources().getBoolean(R.bool
-                .dynamicActionBarColors);
-        boolean dynamicNavBarColors = getResources().getBoolean(R.bool
-                .dynamicNavigationBarColors);
+        boolean dynamicActionBarColors = getResources().getBoolean(R.bool.dynamicActionBarColors);
+        boolean dynamicNavBarColors = getResources().getBoolean(R.bool.dynamicNavigationBarColors);
+
+        // Check if we should apply the new cute font from Google
+        boolean bottomBarUi = !prefs.getBoolean("advanced_ui", false);
+        if (bottomBarUi) {
+            setTheme(R.style.AppTheme_SpecialUI);
+        }
 
         // Obtain the current intent to receive the intent data out of it
         Intent currentIntent = getIntent();

app/src/main/java/projekt/substratum/Substratum.java
@@ -26,8 +26,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.media.AudioAttributes;
 import android.os.Build;
+import android.preference.PreferenceManager;
 import android.support.annotation.RequiresApi;
 import android.util.Log;
 
@@ -99,6 +101,15 @@ public class Substratum extends Application {
     public void onCreate() {
         super.onCreate();
         substratum = this;
+
+        // Set global app theme if on special UI mode
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getInstance());
+        boolean bottomBarUi = !prefs.getBoolean("advanced_ui", false);
+        if (bottomBarUi) {
+            setTheme(R.style.AppTheme_SpecialUI);
+        } else {
+            setTheme(R.style.AppTheme);
+        }
 
         // Firebase
         try {

commit a2e8d0a6e30c4ef0d2cba7d24664e269bc3004ab

ShowcaseTab: Add back alphabetic sorting base code
This code was lost when the new UI was introduced.

Includes: Revert "ShowcaseTab: Remove unused declaration"

Fixes: fcadbe40cb44e9("SubstratumUI: Add in a brand new default, simple UI")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>
Signed-off-by: Ivan Iskandar <ivan@prjkt.io>

app/src/main/java/projekt/substratum/fragments/ShowcaseTab.java
@@ -21,6 +21,8 @@ package projekt.substratum.fragments;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -61,6 +63,7 @@ public class ShowcaseTab extends Fragment {
     RecyclerView mRecyclerView;
     private int current_tab_position;
     private String current_tab_address;
+    private SharedPreferences prefs;
     private Context mContext;
 
     @Override
@@ -69,6 +72,7 @@ public class ShowcaseTab extends Fragment {
             ViewGroup container,
             Bundle savedInstanceState) {
         mContext = Substratum.getInstance();
+        prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
         View view = inflater.inflate(R.layout.showcase_tab, container, false);
         ButterKnife.bind(this, view);
         Bundle bundle = getArguments();
@@ -195,8 +199,12 @@ public class ShowcaseTab extends Fragment {
                 }
                 // Shuffle the deck - every time it will change the order of themes!
                 long seed = System.nanoTime();
-                for (int i = 0; i <= SHOWCASE_SHUFFLE_COUNT; i++)
-                    Collections.shuffle(wallpapers, new Random(seed));
+                boolean alphabetize = showcaseTab.prefs.getBoolean("alphabetize_showcase",
+                        false);
+                if (!alphabetize) {
+                    for (int i = 0; i <= SHOWCASE_SHUFFLE_COUNT; i++)
+                        Collections.shuffle(wallpapers, new Random(seed));
+                }
             }
             return wallpapers;
         }
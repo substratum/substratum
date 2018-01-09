substratum/substratum

commit 702c2f67893920cc98d5f34d97c62c41199d6877

ManagerAdapter: Fix inconsistent labels
Fixes the "Theme: Theme: Ethereal" derp.

Test: Visual
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/adapters/fragments/manager/ManagerAdapter.java
@@ -228,10 +228,7 @@ public class ManagerAdapter extends
                     Typeface.BOLD);
             viewHolder.tvDesc.setText(themeName);
         } else {
-            viewHolder.tvDesc.setText(StringUtils.format(
-                    context.getString(R.string.manager_theme_name),
-                    overlayList.get(position).getThemeName(),
-                    Typeface.BOLD));
+            viewHolder.tvDesc.setText(overlayList.get(position).getThemeName());
         }
 
         viewHolder.chkSelected.setChecked(overlayList.get(position).isSelected());

commit 511c9bcad2ddb1bdf671cf858987f32e5777c638

ManagerAdapter: Fix theme entry formatting
Commit 702c2f67("ManagerAdapter: Fix inconsistent labels")
did fix what it advertised but also uncovered why the
"Theme: themeName" entry was not being stylised as
"<b>Theme</b>: themeName". Rather than doing the stylization
in the ManagerAdapter as we should have, we ended up doing
it in the ManagerItem _and_ calling toString() before we
returned the value from the getThemeName() method which
ended up with all stylization being lost.

Fix this by moving back the formatting into ManagerAdapter.

Fixes: 702c2f67("ManagerAdapter: Fix inconsistent labels")
Fixes: 7a61e1c9("ManagerAdapter: Fix incorrectly styled Theme Name entry")

Test: Visual.
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/adapters/fragments/manager/ManagerAdapter.java
@@ -228,7 +228,10 @@ public class ManagerAdapter extends
                     Typeface.BOLD);
             viewHolder.tvDesc.setText(themeName);
         } else {
-            viewHolder.tvDesc.setText(overlayList.get(position).getThemeName());
+             viewHolder.tvDesc.setText(StringUtils.format(
+                    context.getString(R.string.manager_theme_name),
+                    overlayList.get(position).getThemeName(),
+                    Typeface.BOLD));
         }
 
         viewHolder.chkSelected.setChecked(overlayList.get(position).isSelected());

app/src/main/java/projekt/substratum/adapters/fragments/manager/ManagerItem.java
@@ -162,11 +162,7 @@ public class ManagerItem implements Serializable {
         if (this.themeName == null) {
             this.themeName = this.mContext.getString(R.string.reboot_awaiting_manager_title);
         }
-        return StringUtils.format(
-                mContext.getString(R.string.manager_theme_name),
-                this.themeName,
-                Typeface.BOLD)
-                .toString();
+        return this.themeName;
     }
 
     public void setThemeName(String name) {
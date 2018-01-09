e1d95c7b023271eb05a68287f95a9cb54befe056
Nicholas Chum <nicholas@prjkt.io>
2018-01-10 01:53:14 +0800

    PriorityListFragment: Adapt for Oreo
    - In Oreo, the priority list was reinverted back to normal, with saner logic.
    
    Test: Priorities can be swapped now on Oreo properly
    
    Signed-off-by: Nicholas Chum <nicholas@prjkt.io>

--- app/src/main/java/projekt/substratum/fragments/PriorityListFragment.java
+++ app/src/main/java/projekt/substratum/fragments/PriorityListFragment.java
@@ -19,6 +19,7 @@
 package projekt.substratum.fragments;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
@@ -43,6 +44,7 @@ import com.thesurix.gesturerecycler.GestureAdapter;
 import com.thesurix.gesturerecycler.GestureManager;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import butterknife.BindView;
@@ -155,7 +157,12 @@ public class PriorityListFragment extends Fragment {
                     Packages.getOverlayParentIcon(context, o)));
             workable_list.add(o);
         }
-
+        // On Android Oreo and above, the list is properly reflected now, where the top holds to
+        // highest priority while the bottom holds the lowest, unlike M and N
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Collections.reverse(workable_list);
+            Collections.reverse(prioritiesList);
+        }
         PriorityAdapter adapter = new PriorityAdapter(
                 context,
                 R.layout.priority_overlay_item);

substratum/substratum

commit 1990b7cd500cabefdac79ac469d815503308a1cb

OverlaysManager: If an overlay is about to be recompiled, disable first
To understand the core issue:

Compile an overlay with build and enable, then
with build and update. Said overlay will show as enabled
but would not actually be applied, making target package
appear unthemed.

This appears to be a callbacks handling error from OMS,
which has been rectified in https://substratum.review/#/458.
This patch aims to workaround this bug on stock ROMs.

Test: Running build and update on an already enabled overlay
will update the overlay correctly and the target package
will stay themed.

Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/tabs/OverlaysManager.java
@@ -1290,6 +1290,8 @@ enum OverlaysManager {
                         ArrayList<String> disableBeforeEnabling = new ArrayList<>();
                         List<String> all_installed_overlays = ThemeManager.listAllOverlays
                                 (context);
+                        List<String> all_enabled_overlays = ThemeManager.listOverlays(
+                                context, ThemeManager.STATE_ENABLED);
                         for (String p : all_installed_overlays) {
                             if (!overlays.theme_pid.equals(Packages.
                                     getOverlayParent(context, p))) {
@@ -1298,7 +1300,8 @@ enum OverlaysManager {
                                 for (OverlaysItem oi : overlays.currentInstance.checkedOverlays) {
                                     String targetOverlay = oi.getPackageName();
                                     if (targetOverlay.equals(
-                                            Packages.getOverlayTarget(context, p))) {
+                                            Packages.getOverlayTarget(context, p)) ||
+                                            all_enabled_overlays.contains(targetOverlay)) {
                                         disableBeforeEnabling.add(p);
                                     }
                                 }

commit 2d013ed1b1c5cab830757cce490defe911e1a2bf

OverlaysManager: Correctly disable overlays
Commit 1990b7cd500cabefdac79ac469d815503308a1cb did the
action too late to actually take effect.

Fixes: 1990b7cd500ca ("OverlaysManager: If an overlay is about to be recompiled, disable first")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/tabs/OverlaysManager.java
@@ -494,6 +494,17 @@ enum OverlaysManager {
                         overlays.progressBar.setVisibility(View.VISIBLE);
                         overlays.currentInstance.overlaysWaiting =
                                 overlays.currentInstance.late_install.size();
+                        if (Systems.checkOMS(context)) {
+                            List<String> all_enabled_overlays = ThemeManager
+                                    .listOverlays(context, ThemeManager.STATE_ENABLED);
+                            ArrayList<String> overlaysToDisable = new ArrayList<>();
+                            for (String pkg : overlays.currentInstance.late_install) {
+                                if (all_enabled_overlays.contains(pkg)) {
+                                    overlaysToDisable.add(pkg);
+                                }
+                            }
+                            ThemeManager.disableOverlay(context, overlaysToDisable);
+                        }
                         for (int i = 0; i < overlays.currentInstance.late_install.size(); i++) {
                             ThemeManager.installOverlay(
                                     overlays.getActivity(),
@@ -1290,8 +1301,6 @@ enum OverlaysManager {
                         ArrayList<String> disableBeforeEnabling = new ArrayList<>();
                         List<String> all_installed_overlays = ThemeManager.listAllOverlays
                                 (context);
-                        List<String> all_enabled_overlays = ThemeManager.listOverlays(
-                                context, ThemeManager.STATE_ENABLED);
                         for (String p : all_installed_overlays) {
                             if (!overlays.theme_pid.equals(Packages.
                                     getOverlayParent(context, p))) {
@@ -1300,8 +1309,7 @@ enum OverlaysManager {
                                 for (OverlaysItem oi : overlays.currentInstance.checkedOverlays) {
                                     String targetOverlay = oi.getPackageName();
                                     if (targetOverlay.equals(
-                                            Packages.getOverlayTarget(context, p)) ||
-                                            all_enabled_overlays.contains(targetOverlay)) {
+                                            Packages.getOverlayTarget(context, p))) {
                                         disableBeforeEnabling.add(p);
                                     }
                                 }
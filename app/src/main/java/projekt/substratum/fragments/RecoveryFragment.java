substratum/substratum

commit 8fc23e63226b7540cff19794bcbf4ce1d292482b

SubstratumRebase: The end of sanity
This took far too long...

- Change all Python style naming to camelCase cuz #DisIsJava!
- Migrate a few more fragments to databinding
- Clean things up in binder services
- Simplify things wherever possible
- Move open coded use of bundle extras to use the Internal constants

Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/fragments/RecoveryFragment.java
@@ -21,6 +21,7 @@ package projekt.substratum.fragments;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.databinding.DataBindingUtil;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -28,7 +29,6 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -45,8 +45,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import projekt.substratum.R;
 import projekt.substratum.common.Packages;
 import projekt.substratum.common.References;
@@ -55,6 +53,7 @@ import projekt.substratum.common.Systems;
 import projekt.substratum.common.commands.ElevatedCommands;
 import projekt.substratum.common.commands.FileOperations;
 import projekt.substratum.common.platform.ThemeManager;
+import projekt.substratum.databinding.RestoreFragmentBinding;
 import projekt.substratum.tabs.BootAnimationsManager;
 import projekt.substratum.tabs.FontsManager;
 import projekt.substratum.tabs.WallpapersManager;
@@ -76,24 +75,11 @@ import static projekt.substratum.common.References.VENDOR_DIR;
 
 public class RecoveryFragment extends Fragment {
 
-    @BindView(R.id.overlaysButton)
-    CardView overlaysButton;
-    @BindView(R.id.wallpaperButton)
-    CardView wallpaperButton;
-    @BindView(R.id.bootanimationButton)
-    CardView bootanimationButton;
-    @BindView(R.id.fontsButton)
-    CardView fontsButton;
-    @BindView(R.id.soundsButton)
-    CardView soundsButton;
-    @BindView(R.id.restore_overlay_card)
     View overlayCard;
-    @BindView(R.id.restore_bootanimation_card)
     View bootanimationCard;
-    @BindView(R.id.restore_fonts_card)
     View fontsCard;
-    @BindView(R.id.restore_sounds_card)
     View soundCard;
+    View wallpaperCard;
     private ProgressDialog mProgressDialog;
     private ArrayList<String> final_commands_array;
     private SharedPreferences prefs;
@@ -114,14 +100,22 @@ public class RecoveryFragment extends Fragment {
             Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         context = getContext();
-        View view = inflater.inflate(R.layout.restore_fragment, container, false);
-        ButterKnife.bind(this, view);
+        RestoreFragmentBinding viewBinding =
+                DataBindingUtil.inflate(inflater, R.layout.restore_fragment, container, false);
+        View view = viewBinding.getRoot();
+        overlayCard = viewBinding.restoreOverlayCard;
+        bootanimationCard = viewBinding.restoreBootanimationCard;
+        fontsCard = viewBinding.restoreFontsCard;
+        soundCard = viewBinding.restoreSoundsCard;
+        wallpaperCard = viewBinding.restoreWallpapersCard;
+
+
 
         prefs = PreferenceManager.getDefaultSharedPreferences(context);
         setHasOptionsMenu(true);
 
         // Overlays Dialog
-        overlaysButton.setOnClickListener(v -> {
+        overlayCard.setOnClickListener(v -> {
             sheetDialog = new SheetDialog(context);
             View sheetView = View.inflate(context, R.layout.restore_overlays_sheet_dialog, null);
             LinearLayout disable_all = sheetView.findViewById(R.id.disable_all);
@@ -140,7 +134,7 @@ public class RecoveryFragment extends Fragment {
         });
 
         // Wallpaper Dialog
-        wallpaperButton.setOnClickListener(v -> {
+        wallpaperCard.setOnClickListener(v -> {
             sheetDialog = new SheetDialog(context);
             View sheetView = View.inflate(context, R.layout
                             .restore_wallpapers_sheet_dialog,
@@ -204,7 +198,7 @@ public class RecoveryFragment extends Fragment {
         });
 
         // Boot Animation Dialog
-        bootanimationButton.setOnClickListener(v -> {
+        bootanimationCard.setOnClickListener(v -> {
             sheetDialog = new SheetDialog(context);
             View sheetView = View.inflate(context,
                     R.layout.restore_bootanimations_sheet_dialog, null);
@@ -218,7 +212,7 @@ public class RecoveryFragment extends Fragment {
         });
 
         // Font Dialog
-        fontsButton.setOnClickListener(v -> {
+        fontsCard.setOnClickListener(v -> {
             sheetDialog = new SheetDialog(context);
             View sheetView = View.inflate(context, R.layout.restore_fonts_sheet_dialog, null);
             LinearLayout restore = sheetView.findViewById(R.id.restore);
@@ -234,7 +228,7 @@ public class RecoveryFragment extends Fragment {
         });
 
         // Sounds Dialog
-        soundsButton.setOnClickListener(v -> {
+        soundCard.setOnClickListener(v -> {
             sheetDialog = new SheetDialog(context);
             View sheetView = View.inflate(context, R.layout.restore_sounds_sheet_dialog, null);
             LinearLayout restore = sheetView.findViewById(R.id.restore);

app/src/main/res/layout/restore_fragment.xml
@@ -15,34 +15,37 @@
   ~ You should have received a copy of the GNU General Public License
   ~ along with Substratum.  If not, see <http://www.gnu.org/licenses/>.
   -->
-
-<ScrollView xmlns:android="http://schemas.android.com/apk/res/android"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent">
-
-    <LinearLayout
+<layout xmlns:android="http://schemas.android.com/apk/res/android">
+    <ScrollView
         android:layout_width="match_parent"
-        android:layout_height="wrap_content"
-        android:layout_marginTop="5dp"
-        android:orientation="vertical">
+        android:layout_height="match_parent">
 
-        <include
-            android:id="@+id/restore_overlay_card"
-            layout="@layout/restore_overlay" />
+        <LinearLayout
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:layout_marginTop="5dp"
+            android:orientation="vertical">
 
-        <include
-            android:id="@+id/restore_bootanimation_card"
-            layout="@layout/restore_bootanimation" />
+            <include
+                android:id="@+id/restore_overlay_card"
+                layout="@layout/restore_overlay" />
 
-        <include
-            android:id="@+id/restore_fonts_card"
-            layout="@layout/restore_fonts" />
+            <include
+                android:id="@+id/restore_bootanimation_card"
+                layout="@layout/restore_bootanimation" />
 
-        <include
-            android:id="@+id/restore_sounds_card"
-            layout="@layout/restore_sounds" />
+            <include
+                android:id="@+id/restore_fonts_card"
+                layout="@layout/restore_fonts" />
 
-        <include layout="@layout/restore_wallpaper" />
+            <include
+                android:id="@+id/restore_sounds_card"
+                layout="@layout/restore_sounds" />
 
-    </LinearLayout>
-</ScrollView>
+            <include
+                android:id="@+id/restore_wallpapers_card"
+                layout="@layout/restore_wallpaper" />
+
+        </LinearLayout>
+    </ScrollView>
+</layout>

commit 9d3c2946724cac5d05a6b865194d8ecb3f16eab8

RecoveryFragment: Revert databinding migration
My method of doing it broke the onClick behavior.
Reverting until someone finds a better way to do this.

Fixes: 8fc23e6322 ("SubstratumRebase: The end of sanity")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/fragments/RecoveryFragment.java
@@ -21,7 +21,6 @@ package projekt.substratum.fragments;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.databinding.DataBindingUtil;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -29,6 +28,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -45,6 +45,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import projekt.substratum.R;
 import projekt.substratum.common.Packages;
 import projekt.substratum.common.References;
@@ -53,7 +55,6 @@ import projekt.substratum.common.Systems;
 import projekt.substratum.common.commands.ElevatedCommands;
 import projekt.substratum.common.commands.FileOperations;
 import projekt.substratum.common.platform.ThemeManager;
-import projekt.substratum.databinding.RestoreFragmentBinding;
 import projekt.substratum.tabs.BootAnimationsManager;
 import projekt.substratum.tabs.FontsManager;
 import projekt.substratum.tabs.WallpapersManager;
@@ -75,11 +76,24 @@ import static projekt.substratum.common.References.VENDOR_DIR;
 
 public class RecoveryFragment extends Fragment {
 
+    @BindView(R.id.overlaysButton)
+    CardView overlaysButton;
+    @BindView(R.id.wallpaperButton)
+    CardView wallpaperButton;
+    @BindView(R.id.bootanimationButton)
+    CardView bootanimationButton;
+    @BindView(R.id.fontsButton)
+    CardView fontsButton;
+    @BindView(R.id.soundsButton)
+    CardView soundsButton;
+    @BindView(R.id.restore_overlay_card)
     View overlayCard;
+    @BindView(R.id.restore_bootanimation_card)
     View bootanimationCard;
+    @BindView(R.id.restore_fonts_card)
     View fontsCard;
+    @BindView(R.id.restore_sounds_card)
     View soundCard;
-    View wallpaperCard;
     private ProgressDialog mProgressDialog;
     private ArrayList<String> final_commands_array;
     private SharedPreferences prefs;
@@ -100,22 +114,14 @@ public class RecoveryFragment extends Fragment {
             Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         context = getContext();
-        RestoreFragmentBinding viewBinding =
-                DataBindingUtil.inflate(inflater, R.layout.restore_fragment, container, false);
-        View view = viewBinding.getRoot();
-        overlayCard = viewBinding.restoreOverlayCard;
-        bootanimationCard = viewBinding.restoreBootanimationCard;
-        fontsCard = viewBinding.restoreFontsCard;
-        soundCard = viewBinding.restoreSoundsCard;
-        wallpaperCard = viewBinding.restoreWallpapersCard;
-
-
+        View view = inflater.inflate(R.layout.restore_fragment, container, false);
+        ButterKnife.bind(this, view);
 
         prefs = PreferenceManager.getDefaultSharedPreferences(context);
         setHasOptionsMenu(true);
 
         // Overlays Dialog
-        overlayCard.setOnClickListener(v -> {
+        overlaysButton.setOnClickListener(v -> {
             sheetDialog = new SheetDialog(context);
             View sheetView = View.inflate(context, R.layout.restore_overlays_sheet_dialog, null);
             LinearLayout disable_all = sheetView.findViewById(R.id.disable_all);
@@ -134,7 +140,7 @@ public class RecoveryFragment extends Fragment {
         });
 
         // Wallpaper Dialog
-        wallpaperCard.setOnClickListener(v -> {
+        wallpaperButton.setOnClickListener(v -> {
             sheetDialog = new SheetDialog(context);
             View sheetView = View.inflate(context, R.layout
                             .restore_wallpapers_sheet_dialog,
@@ -198,7 +204,7 @@ public class RecoveryFragment extends Fragment {
         });
 
         // Boot Animation Dialog
-        bootanimationCard.setOnClickListener(v -> {
+        bootanimationButton.setOnClickListener(v -> {
             sheetDialog = new SheetDialog(context);
             View sheetView = View.inflate(context,
                     R.layout.restore_bootanimations_sheet_dialog, null);
@@ -212,7 +218,7 @@ public class RecoveryFragment extends Fragment {
         });
 
         // Font Dialog
-        fontsCard.setOnClickListener(v -> {
+        fontsButton.setOnClickListener(v -> {
             sheetDialog = new SheetDialog(context);
             View sheetView = View.inflate(context, R.layout.restore_fonts_sheet_dialog, null);
             LinearLayout restore = sheetView.findViewById(R.id.restore);
@@ -228,7 +234,7 @@ public class RecoveryFragment extends Fragment {
         });
 
         // Sounds Dialog
-        soundCard.setOnClickListener(v -> {
+        soundsButton.setOnClickListener(v -> {
             sheetDialog = new SheetDialog(context);
             View sheetView = View.inflate(context, R.layout.restore_sounds_sheet_dialog, null);
             LinearLayout restore = sheetView.findViewById(R.id.restore);

app/src/main/res/layout/restore_fragment.xml
@@ -15,37 +15,34 @@
   ~ You should have received a copy of the GNU General Public License
   ~ along with Substratum.  If not, see <http://www.gnu.org/licenses/>.
   -->
-<layout xmlns:android="http://schemas.android.com/apk/res/android">
-    <ScrollView
+
+<ScrollView xmlns:android="http://schemas.android.com/apk/res/android"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent">
+
+    <LinearLayout
         android:layout_width="match_parent"
-        android:layout_height="match_parent">
+        android:layout_height="wrap_content"
+        android:layout_marginTop="5dp"
+        android:orientation="vertical">
 
-        <LinearLayout
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="5dp"
-            android:orientation="vertical">
+        <include
+            android:id="@+id/restore_overlay_card"
+            layout="@layout/restore_overlay" />
 
-            <include
-                android:id="@+id/restore_overlay_card"
-                layout="@layout/restore_overlay" />
+        <include
+            android:id="@+id/restore_bootanimation_card"
+            layout="@layout/restore_bootanimation" />
 
-            <include
-                android:id="@+id/restore_bootanimation_card"
-                layout="@layout/restore_bootanimation" />
+        <include
+            android:id="@+id/restore_fonts_card"
+            layout="@layout/restore_fonts" />
 
-            <include
-                android:id="@+id/restore_fonts_card"
-                layout="@layout/restore_fonts" />
+        <include
+            android:id="@+id/restore_sounds_card"
+            layout="@layout/restore_sounds" />
 
-            <include
-                android:id="@+id/restore_sounds_card"
-                layout="@layout/restore_sounds" />
+        <include layout="@layout/restore_wallpaper" />
 
-            <include
-                android:id="@+id/restore_wallpapers_card"
-                layout="@layout/restore_wallpaper" />
-
-        </LinearLayout>
-    </ScrollView>
-</layout>
+    </LinearLayout>
+</ScrollView>
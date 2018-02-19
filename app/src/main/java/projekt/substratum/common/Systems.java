substratum/substratum

commit e89d368dc147f4b8b4ac2f47af82d80b3ddb6d3a

Clean-up and reformat
- Remove unused methods left behind by ce997b463363
  and ac984631f8ed.
- Get rid of some lint suppression and document others
  wherever required
- Ensure debugging overrides that were tripping lint
  are only added on debug builds

Fixes: ce997b463363 ("SubstratumRebase: Kill off old UI")
Fixes: ac984631f8ed ("ThemeAdapter: Migrate to databinding and deprecate gapps config")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/common/Systems.java
@@ -108,6 +108,10 @@ public enum Systems {
      * @param context Self explanatory, sir
      */
     public static Boolean checkOMS(@NonNull Context context) {
+        // In an ideal world lint would know that
+        // OMS refreshes kill kittens and contexts.
+        // But we don't live in an ideal world so
+        // will have to do.
         //noinspection ConstantConditions
         if (context == null) return true; // Safe to assume that window refreshes only on OMS
         if (!BYPASS_SYSTEM_VERSION_CHECK) {
@@ -606,7 +610,6 @@ public enum Systems {
                     "com.forpda.",
                     "zone.jasi2169."
             };
-            //noinspection ConstantConditions
             checkPackageSupported = blacklistedPackages.length != 0 && (
                     checkPackageRegex(context, blacklistedPackages) ||
                             spreadYourWingsAndFly(context, override) ||

app/src/main/java/projekt/substratum/MainActivity.java
@@ -259,11 +259,10 @@ public class MainActivity extends AppCompatActivity implements
     /**
      * Transact to the license fragment
      *
-     * @param title    Fragment's title
      * @param fragment Name of the fragment in projekt.substratum.fragments
      */
-    private void switchFragmentToLicenses(CharSequence title,
-                                          LibsSupportFragment fragment) {
+    @SuppressWarnings("unused")
+    private void switchFragmentToLicenses(LibsSupportFragment fragment) {
         if ((searchView != null) && !searchView.isIconified()) {
             searchView.setIconified(true);
         }
@@ -1015,8 +1014,9 @@ public class MainActivity extends AppCompatActivity implements
             // These are hardcoded booleans and lint is squawking because
             // of the hardcode. The utility of these is mentioned alongside
             // their declarations.
-            //noinspection ConstantConditions
-            dialogReturnBool &= ENABLE_ROOT_CHECK & !BYPASS_SYSTEM_VERSION_CHECK;
+            // Only enable these on debug builds
+            if (BuildConfig.DEBUG)
+                dialogReturnBool &= ENABLE_ROOT_CHECK & !BYPASS_SYSTEM_VERSION_CHECK;
 
             super.onPostExecute(dialogReturnBool);
             MainActivity activity = ref.get();

app/src/main/java/projekt/substratum/common/Internal.java
@@ -105,11 +105,8 @@ public enum Internal {
     public static final String EFFECT_TICK = "Effect_Tick";
     public static final String LOCK = "Lock";
     public static final String UNLOCK = "Unlock";
-    // Theme Ready
+    // For Play store links
     public static final String PLAY_URL_PREFIX = "https://play.google.com/store/apps/details?id=";
-    public static final String THEME_READY_ALL = "all";
-    public static final String THEME_READY_READY = "ready";
-    public static final String THEME_READY_STOCK = "stock";
     // Misc Packages
     public static final String PHONE = "com.android.phone";
     public static final String PHONE_COMMON_FRAMEWORK = "com.android.phone.common";

app/src/main/java/projekt/substratum/common/Packages.java
@@ -33,7 +33,6 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.VectorDrawable;
-import android.os.Build;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
@@ -70,7 +69,6 @@ import static projekt.substratum.common.References.metadataOverlayParent;
 import static projekt.substratum.common.References.metadataOverlayTarget;
 import static projekt.substratum.common.References.metadataOverlayVersion;
 import static projekt.substratum.common.References.metadataSamsungSupport;
-import static projekt.substratum.common.References.metadataThemeReady;
 import static projekt.substratum.common.References.metadataVersion;
 import static projekt.substratum.common.References.resourceChangelog;
 import static projekt.substratum.common.Resources.FRAMEWORK;
@@ -106,8 +104,7 @@ public enum Packages {
         try {
             PackageManager pm = context.getPackageManager();
             return pm.getInstallerPackageName(packageName);
-        } catch (Exception e) {
-            // Suppress warning
+        } catch (Exception ignored) {
         }
         return null;
     }
@@ -228,6 +225,9 @@ public enum Packages {
             bitmap = getBitmapFromVector(drawable);
         } else if ((drawable instanceof BitmapDrawable)
                 | (drawable instanceof ShapeDrawable)) {
+            // Lint doesn't realise that ShapeDrawable and
+            // BitmapDrawable are interoperable classes so
+            // keeps throwing the warning.
             //noinspection ConstantConditions
             bitmap = ((BitmapDrawable) drawable).getBitmap();
         } else if (drawable instanceof AdaptiveIconDrawable) {
@@ -402,113 +402,6 @@ public enum Packages {
             // Suppress warning
         }
         return 0;
-    }
-
-    /**
-     * Grabs a given theme's app version
-     *
-     * @param context     Context
-     * @param packageName Package name of the desired app to be checked
-     * @return Returns a string of the app's version
-     */
-    public static String getThemeVersion(Context context,
-                                         String packageName) {
-        try {
-            PackageInfo pInfo = context.getPackageManager().getPackageInfo(packageName, 0);
-            return pInfo.versionName + " (" + pInfo.versionCode + ')';
-        } catch (PackageManager.NameNotFoundException e) {
-            // Suppress warning
-        }
-        return null;
-    }
-
-    /**
-     * Grab the available API levels for a given theme
-     *
-     * @param context     Context
-     * @param packageName Package name of the desired app to be checked
-     * @return Returns a string of the theme's SDK APIs
-     */
-    public static String getThemeAPIs(Context context,
-                                      String packageName) {
-        try {
-            ApplicationInfo appInfo = context.getPackageManager().getApplicationInfo(
-                    packageName, PackageManager.GET_META_DATA);
-            if (appInfo.metaData != null) {
-                try {
-                    if (appInfo.minSdkVersion == appInfo.targetSdkVersion) {
-                        int target = appInfo.targetSdkVersion;
-                        switch (target) {
-                            case Build.VERSION_CODES.N:
-                                return context.getString(R.string.api_24);
-                            case Build.VERSION_CODES.N_MR1:
-                                return context.getString(R.string.api_25);
-                            case Build.VERSION_CODES.O:
-                                return context.getString(R.string.api_26);
-                            case Build.VERSION_CODES.O_MR1:
-                                return context.getString(R.string.api_27);
-                        }
-                    } else {
-                        String minSdk = "";
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                            int min = appInfo.minSdkVersion;
-                            switch (min) {
-                                case Build.VERSION_CODES.N:
-                                    minSdk = context.getString(R.string.api_24);
-                                    break;
-                                case Build.VERSION_CODES.N_MR1:
-                                    minSdk = context.getString(R.string.api_25);
-                                    break;
-                                case Build.VERSION_CODES.O:
-                                    minSdk = context.getString(R.string.api_26);
-                                    break;
-                                case Build.VERSION_CODES.O_MR1:
-                                    minSdk = context.getString(R.string.api_27);
-                                    break;
-                            }
-                        }
-                        String targetSdk = "";
-                        int target = appInfo.targetSdkVersion;
-                        switch (target) {
-                            case Build.VERSION_CODES.N:
-                                targetSdk = context.getString(R.string.api_24);
-                                break;
-                            case Build.VERSION_CODES.N_MR1:
-                                targetSdk = context.getString(R.string.api_25);
-                                break;
-                            case Build.VERSION_CODES.O:
-                                targetSdk = context.getString(R.string.api_26);
-                                break;
-                            case Build.VERSION_CODES.O_MR1:
-                                targetSdk = context.getString(R.string.api_27);
-                                break;
-                        }
-                        return minSdk + " - " + targetSdk;
-                    }
-                } catch (NoSuchFieldError noSuchFieldError) {
-                    int target = appInfo.targetSdkVersion;
-                    if (target == Build.VERSION_CODES.N) {
-                        return context.getString(R.string.api_24);
-                    } else {
-                        String targetAPI = "";
-                        switch (target) {
-                            case Build.VERSION_CODES.N_MR1:
-                                targetAPI = context.getString(R.string.api_25);
-                                break;
-                            case Build.VERSION_CODES.O:
-                                targetAPI = context.getString(R.string.api_26);
-                                break;
-                            case Build.VERSION_CODES.O_MR1:
-                                targetAPI = context.getString(R.string.api_27);
-                        }
-                        return context.getString(R.string.api_24) + " - " + targetAPI;
-                    }
-                }
-            }
-        } catch (Exception e) {
-            // Suppress warning
-        }
-        return null;
     }
 
     /**
@@ -689,18 +582,6 @@ public enum Packages {
     }
 
     /**
-     * Get Theme Ready support
-     *
-     * @param context     Context
-     * @param packageName Package name of the desired app to be checked
-     * @return String of whether the theme supports theme ready
-     */
-    public static String getThemeReadyVisibility(Context context,
-                                                 String packageName) {
-        return getOverlayMetadata(context, packageName, metadataThemeReady);
-    }
-
-    /**
      * Get theme plugin version
      *
      * @param context     Context
@@ -796,7 +677,6 @@ public enum Packages {
      * Checks the packages for Substratum, then mutates the input
      *
      * @param context      Context
-     * @param homeType     Home type
      * @param searchFilter User input in search
      * @return Returns a map of substratum ready packages
      */
@@ -938,8 +818,7 @@ public enum Packages {
             PackageManager pm = context.getPackageManager();
             ApplicationInfo ai = pm.getApplicationInfo(packageName, 0);
             return ai.sourceDir;
-        } catch (Exception e) {
-            // Suppress warning
+        } catch (Exception ignored) {
         }
         return null;
     }

app/src/main/java/projekt/substratum/common/Restore.java
@@ -80,14 +80,14 @@ public enum Restore {
                 PreferenceManager.getDefaultSharedPreferences(context);
         String selectedTheme = prefs.getString(APP_THEME, DEFAULT_THEME);
 
-        LinearLayout disable_all = sheetView.findViewById(R.id.disable_all);
-        LinearLayout uninstall_all = sheetView.findViewById(R.id.uninstall_all);
-        LinearLayout restore_bootanimation = sheetView.findViewById(R.id.restore_bootanimation);
-        LinearLayout restore_system_font = sheetView.findViewById(R.id.restore_system_font);
-        LinearLayout restore_sounds = sheetView.findViewById(R.id.restore_sounds);
-        LinearLayout home_wallpaper = sheetView.findViewById(R.id.home_wallpaper);
-        LinearLayout lock_wallpaper = sheetView.findViewById(R.id.lock_wallpaper);
-        LinearLayout both_wallpapers = sheetView.findViewById(R.id.both_wallpapers);
+        LinearLayout disableAll = sheetView.findViewById(R.id.disable_all);
+        LinearLayout uninstallAll = sheetView.findViewById(R.id.uninstall_all);
+        LinearLayout restoreBootanimation = sheetView.findViewById(R.id.restore_bootanimation);
+        LinearLayout restoreSystemFont = sheetView.findViewById(R.id.restore_system_font);
+        LinearLayout restoreSounds = sheetView.findViewById(R.id.restore_sounds);
+        LinearLayout homeWallpaper = sheetView.findViewById(R.id.home_wallpaper);
+        LinearLayout lockWallpaper = sheetView.findViewById(R.id.lock_wallpaper);
+        LinearLayout bothWallpapers = sheetView.findViewById(R.id.both_wallpapers);
 
         // Doing this because for some reason the dark theme does not take charge
         if (context.getResources().getBoolean(R.bool.forceAppDarkTheme) ||
@@ -116,40 +116,40 @@ public enum Restore {
                                 context.getColor(R.color.bottom_sheet_dialog_icons_night)
                         });
 
-                int text_tint = context.getColor(R.color.bottom_sheet_dialog_text_night);
+                int textTint = context.getColor(R.color.bottom_sheet_dialog_text_night);
 
-                TextView disable = disable_all.findViewById(R.id.disable_text);
-                disable.setTextColor(text_tint);
+                TextView disable = disableAll.findViewById(R.id.disable_text);
+                disable.setTextColor(textTint);
                 disable.setCompoundDrawableTintList(icon_tint);
 
-                TextView uninstall = uninstall_all.findViewById(R.id.uninstall_text);
-                uninstall.setTextColor(text_tint);
+                TextView uninstall = uninstallAll.findViewById(R.id.uninstall_text);
+                uninstall.setTextColor(textTint);
                 uninstall.setCompoundDrawableTintList(icon_tint);
 
                 TextView bootanimation =
-                        restore_bootanimation.findViewById(R.id.restore_bootanimation_text);
-                bootanimation.setTextColor(text_tint);
+                        restoreBootanimation.findViewById(R.id.restore_bootanimation_text);
+                bootanimation.setTextColor(textTint);
                 bootanimation.setCompoundDrawableTintList(icon_tint);
 
-                TextView sounds = restore_sounds.findViewById(R.id.restore_sounds_text);
-                sounds.setTextColor(text_tint);
+                TextView sounds = restoreSounds.findViewById(R.id.restore_sounds_text);
+                sounds.setTextColor(textTint);
                 sounds.setCompoundDrawableTintList(icon_tint);
 
-                TextView fonts = restore_system_font.findViewById(R.id.restore_font_text);
-                fonts.setTextColor(text_tint);
+                TextView fonts = restoreSystemFont.findViewById(R.id.restore_font_text);
+                fonts.setTextColor(textTint);
                 fonts.setCompoundDrawableTintList(icon_tint);
 
-                TextView home_wall = home_wallpaper.findViewById(R.id.restore_home_wallpaper_text);
-                home_wall.setTextColor(text_tint);
+                TextView home_wall = homeWallpaper.findViewById(R.id.restore_home_wallpaper_text);
+                home_wall.setTextColor(textTint);
                 home_wall.setCompoundDrawableTintList(icon_tint);
 
-                TextView lock_wall = lock_wallpaper.findViewById(R.id.restore_lock_wallpaper_text);
-                lock_wall.setTextColor(text_tint);
+                TextView lock_wall = lockWallpaper.findViewById(R.id.restore_lock_wallpaper_text);
+                lock_wall.setTextColor(textTint);
                 lock_wall.setCompoundDrawableTintList(icon_tint);
 
                 TextView both_walls =
-                        both_wallpapers.findViewById(R.id.restore_both_wallpapers_text);
-                both_walls.setTextColor(text_tint);
+                        bothWallpapers.findViewById(R.id.restore_both_wallpapers_text);
+                both_walls.setTextColor(textTint);
                 both_walls.setCompoundDrawableTintList(icon_tint);
             }
         }
@@ -157,32 +157,32 @@ public enum Restore {
         View view = activity.findViewById(R.id.drawer_container);
 
         if (!Systems.checkOMS(context)) {
-            disable_all.setVisibility(View.GONE);
-            uninstall_all.setVisibility(View.GONE);
+            disableAll.setVisibility(View.GONE);
+            uninstallAll.setVisibility(View.GONE);
         }
-        if (!isBootAnimationSupported(context)) restore_bootanimation.setVisibility(View.GONE);
-        if (!isFontsSupported(context)) restore_system_font.setVisibility(View.GONE);
-        if (!isSoundsSupported(context)) restore_sounds.setVisibility(View.GONE);
+        if (!isBootAnimationSupported(context)) restoreBootanimation.setVisibility(View.GONE);
+        if (!isFontsSupported(context)) restoreSystemFont.setVisibility(View.GONE);
+        if (!isSoundsSupported(context)) restoreSounds.setVisibility(View.GONE);
 
         // Overlays
-        disable_all.setOnClickListener(vi -> {
+        disableAll.setOnClickListener(vi -> {
             new RestoreFunction(activity).execute(false);
             sheetDialog.hide();
         });
-        uninstall_all.setOnClickListener(vi -> {
+        uninstallAll.setOnClickListener(vi -> {
             new RestoreFunction(activity).execute(true);
             sheetDialog.hide();
         });
 
 
         // Boot Animations
-        restore_bootanimation.setOnClickListener(view2 -> {
+        restoreBootanimation.setOnClickListener(view2 -> {
             new BootAnimationClearer(activity).execute();
             sheetDialog.hide();
         });
 
         // Fonts
-        restore_system_font.setOnClickListener(view2 -> {
+        restoreSystemFont.setOnClickListener(view2 -> {
             if (Systems.checkThemeInterfacer(context) ||
                     Systems.checkSubstratumService(context)) {
                 new FontsClearer(activity).execute("");
@@ -191,7 +191,7 @@ public enum Restore {
         });
 
         // Sounds
-        restore_sounds.setOnClickListener(view2 -> {
+        restoreSounds.setOnClickListener(view2 -> {
             if (Systems.checkThemeInterfacer(context) ||
                     Systems.checkSubstratumService(context)) {
                 new SoundsClearer(activity).execute();
@@ -200,7 +200,7 @@ public enum Restore {
         });
 
         // Wallpapers
-        home_wallpaper.setOnClickListener(view2 -> {
+        homeWallpaper.setOnClickListener(view2 -> {
             try {
                 WallpapersManager.clearWallpaper(context, HOME_WALLPAPER);
                 if (view != null) {
@@ -218,7 +218,7 @@ public enum Restore {
             }
             sheetDialog.hide();
         });
-        lock_wallpaper.setOnClickListener(view2 -> {
+        lockWallpaper.setOnClickListener(view2 -> {
             try {
                 WallpapersManager.clearWallpaper(context, LOCK_WALLPAPER);
                 if (view != null) {
@@ -233,7 +233,7 @@ public enum Restore {
             }
             sheetDialog.hide();
         });
-        both_wallpapers.setOnClickListener(view2 -> {
+        bothWallpapers.setOnClickListener(view2 -> {
             try {
                 WallpapersManager.clearWallpaper(context, ALL_WALLPAPER);
                 if (view != null) {
@@ -259,7 +259,7 @@ public enum Restore {
      * Uninstall overlays
      */
     private static class RestoreFunction extends AsyncTask<Boolean, Void, Void> {
-        ArrayList<String> final_commands_array;
+        ArrayList<String> finalCommandsArray;
         private WeakReference<Activity> ref;
         private boolean withUninstall;
         private ProgressDialog alertDialog;
@@ -302,7 +302,7 @@ public enum Restore {
                             //noinspection ConstantConditions
                             if (context != null) {
                                 ThemeManager.uninstallOverlay(
-                                        context, final_commands_array);
+                                        context, finalCommandsArray);
                             }
                         } catch (Exception e) {
                             // At this point the window is refreshed too many times detaching the
@@ -355,8 +355,8 @@ public enum Restore {
                     if (Systems.checkOMS(context)) {
                         List<String> overlays = ThemeManager.listAllOverlays(context);
 
-                        final_commands_array = new ArrayList<>();
-                        final_commands_array.addAll(overlays.stream()
+                        finalCommandsArray = new ArrayList<>();
+                        finalCommandsArray.addAll(overlays.stream()
                                 .filter(o -> Packages.getOverlayParent(context, o) != null)
                                 .collect(Collectors.toList()));
                     } else {

app/src/main/java/projekt/substratum/common/commands/CompilerCommands.java
@@ -94,8 +94,10 @@ public enum CompilerCommands {
         if (!baseVariantNull) packageName = packageName + variantName + baseVariantName;
         if (isNotNullOrEmpty(packageNameOverride)) packageName = packageNameOverride;
 
-        boolean showOverlayInSamsungSettings =
-                Systems.isSamsungDevice(context) && References.toggleShowSamsungOverlayInSettings;
+        boolean showOverlayInSamsungSettings = Systems.isSamsungDevice(context);
+        if (BuildConfig.DEBUG)
+            showOverlayInSamsungSettings &= References.toggleShowSamsungOverlayInSettings;
+
         try {
             DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
                     .newInstance();

app/src/main/java/projekt/substratum/fragments/SettingsFragment.java
@@ -44,6 +44,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -950,12 +951,14 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 Button button = dialog2.findViewById(R.id.button_done);
                 button.setOnClickListener(v -> dialog2.dismiss());
 
-                WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
-                //noinspection ConstantConditions
-                layoutParams.copyFrom(dialog2.getWindow().getAttributes());
-                layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
-                layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT;
-                dialog2.getWindow().setAttributes(layoutParams);
+                Window window = dialog2.getWindow();
+                if (window != null) {
+                    WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+                    layoutParams.copyFrom(window.getAttributes());
+                    layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+                    layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT;
+                    window.setAttributes(layoutParams);
+                }
                 dialog2.show();
             }
         }

app/src/main/java/projekt/substratum/services/floatui/SubstratumFloatInterface.java
@@ -238,16 +238,16 @@ public class SubstratumFloatInterface extends Service implements FloatingViewLis
                 mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
                 AlertDialog alertDialog = builder.create();
-                //noinspection ConstantConditions
-                alertDialog.getWindow().setBackgroundDrawable(
+                Window window = alertDialog.getWindow();
+                assert window != null;
+                window.setBackgroundDrawable(
                         getDrawable(R.drawable.dialog_background));
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    alertDialog.getWindow().setType(LayoutParams.TYPE_APPLICATION_OVERLAY);
+                    window.setType(LayoutParams.TYPE_APPLICATION_OVERLAY);
                 } else {
-                    alertDialog.getWindow().setType(LayoutParams.TYPE_SYSTEM_ALERT);
+                    window.setType(LayoutParams.TYPE_SYSTEM_ALERT);
                 }
 
-                Window window = alertDialog.getWindow();
                 WindowManager.LayoutParams windowParams = window.getAttributes();
                 windowParams.copyFrom(window.getAttributes());
                 windowParams.width = LayoutParams.MATCH_PARENT;

commit 8b808faf8fa207126fb78ef6dea5a1eb08c00813

Systems: Remove NonNull annotation from checkOMS method
Context _can_ be null especially with the nature of how
window refreshes via OMS work. Drop the annotation and
accompanying lint suppression and explanation since
they're not relevant anymore.

Thanks to @jereksel for the hint.

Fixes: e89d368dc147 ("Clean-up and reformat")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/common/Systems.java
@@ -31,7 +31,6 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
-import android.support.annotation.NonNull;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -107,12 +106,7 @@ public enum Systems {
      *
      * @param context Self explanatory, sir
      */
-    public static Boolean checkOMS(@NonNull Context context) {
-        // In an ideal world lint would know that
-        // OMS refreshes kill kittens and contexts.
-        // But we don't live in an ideal world so
-        // will have to do.
-        //noinspection ConstantConditions
+    public static Boolean checkOMS(Context context) {
         if (context == null) return true; // Safe to assume that window refreshes only on OMS
         if (!BYPASS_SYSTEM_VERSION_CHECK) {
             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
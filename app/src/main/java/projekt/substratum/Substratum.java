substratum/substratum

commit 3f66c887895ac1d9a5a84fb78b86e469d7492131

treewide: Restrict debug logging to debug builds
Stop spamming logcats of production users.

Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/Substratum.java
@@ -131,7 +131,7 @@ public class Substratum extends Application {
      * Stop the ongoing package detection on Samsung
      */
     static void stopSamsungPackageMonitor() {
-        Log.d("Substratum",
+        log("Substratum",
                 "The overlay package refresher for Samsung devices is now stopping!");
         shouldStopThread = true;
     }
@@ -142,7 +142,7 @@ public class Substratum extends Application {
      * @param context Context!
      */
     static void startSamsungPackageMonitor(Context context) {
-        Log.d("Substratum",
+        log("Substratum",
                 "The overlay package refresher for Samsung devices has been fully loaded.");
         PackageManager pm = context.getPackageManager();
         List<ApplicationInfo> currentApps =
@@ -208,6 +208,12 @@ public class Substratum extends Application {
         return weakSelf.get().preferences;
     }
 
+    public static void log(final String TAG, final String message) {
+        if (BuildConfig.DEBUG)
+            return;
+        Log.d(TAG, message);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -242,7 +248,7 @@ public class Substratum extends Application {
         // Dynamically check which theme engine is running at the moment
         if (isAndromedaDevice(this.getApplicationContext())) {
             boolean startBinderService = this.startBinderService(AndromedaBinderService.class);
-            Log.d(BINDER_TAG, "Successful to start the Andromeda binder service: " +
+            log(BINDER_TAG, "Successful to start the Andromeda binder service: " +
                     (startBinderService ? "Success!" : "Failed"));
             if (!startBinderService) {
                 this.stopService(
@@ -250,7 +256,7 @@ public class Substratum extends Application {
             }
         } else if (isBinderInterfacer(this.getApplicationContext())) {
             boolean startBinderService = this.startBinderService(InterfacerBinderService.class);
-            Log.d(BINDER_TAG, "Successful to start the Interfacer binder service: " +
+            log(BINDER_TAG, "Successful to start the Interfacer binder service: " +
                     (startBinderService ? "Success!" : "Failed"));
             if (!startBinderService) {
                 this.stopService(
@@ -325,19 +331,19 @@ public class Substratum extends Application {
         try {
             if (className.equals(AndromedaBinderService.class)) {
                 if (this.checkServiceActivation(AndromedaBinderService.class)) {
-                    Log.d(BINDER_TAG,
+                    log(BINDER_TAG,
                             "This session will utilize the connected Andromeda Binder service!");
                 } else {
-                    Log.d(BINDER_TAG,
+                    log(BINDER_TAG,
                             "Substratum is now connecting to the Andromeda Binder service...");
                     this.startService(new Intent(this.getApplicationContext(),
                             AndromedaBinderService.class));
                 }
             } else if (className.equals(InterfacerBinderService.class)) {
                 if (this.checkServiceActivation(InterfacerBinderService.class)) {
-                    Log.d(BINDER_TAG, "This session will utilize the connected Binder service!");
+                    log(BINDER_TAG, "This session will utilize the connected Binder service!");
                 } else {
-                    Log.d(BINDER_TAG, "Substratum is now connecting to the Binder service...");
+                    log(BINDER_TAG, "Substratum is now connecting to the Binder service...");
                     Intent i = new Intent(this.getApplicationContext(),
                             InterfacerBinderService.class);
                     this.startService(i);
@@ -400,7 +406,7 @@ public class Substratum extends Application {
                 String check = Packages.getOverlayParent(context, packageName);
                 if (check != null) {
                     isWaiting = false;
-                    Log.d("Substratum", "PACKAGE_ADDED: " + packageName);
+                    log("Substratum", "PACKAGE_ADDED: " + packageName);
                 }
             }
         }

app/src/main/java/projekt/substratum/InformationActivity.java
@@ -75,6 +75,7 @@ import com.gordonwong.materialsheetfab.MaterialSheetFab;
 import com.gordonwong.materialsheetfab.MaterialSheetFabEventListener;
 import com.theartofdev.edmodo.cropper.CropImage;
 import org.apache.commons.io.output.ByteArrayOutputStream;
+import projekt.substratum.Substratum;
 import projekt.substratum.adapters.activities.IATabsAdapter;
 import projekt.substratum.common.Broadcasts;
 import projekt.substratum.common.Internal;
@@ -1231,7 +1232,7 @@ public class InformationActivity extends AppCompatActivity {
                     context.getCacheDir().getAbsolutePath();
             File deleted = new File(workingDirectory);
             FileOperations.delete(context, deleted.getAbsolutePath());
-            if (!deleted.exists()) Log.d(References.SUBSTRATUM_BUILDER,
+            if (!deleted.exists()) Substratum.log(References.SUBSTRATUM_BUILDER,
                     "Successfully cleared Substratum cache!");
         }
     }
@@ -1449,7 +1450,7 @@ public class InformationActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (!Packages.isPackageInstalled(context, themePid)) {
-                Log.d("ThemeUninstaller",
+                Substratum.log("ThemeUninstaller",
                         "The theme was uninstalled, so the activity is now closing!");
                 Broadcasts.sendRefreshMessage(context);
                 finish();
@@ -1480,7 +1481,7 @@ public class InformationActivity extends AppCompatActivity {
                         packageName.equals(themePid)) {
                     String themeUpdatedToastText = String.format(
                             getString(R.string.toast_activity_finished), themeName);
-                    Log.d(SUBSTRATUM_LOG,
+                    Substratum.log(SUBSTRATUM_LOG,
                             themeName + " was just updated, now closing InformationActivity...");
                     Toast.makeText(context, themeUpdatedToastText, Toast.LENGTH_LONG).show();
                     finish();
@@ -1488,7 +1489,7 @@ public class InformationActivity extends AppCompatActivity {
                             Theming.launchTheme(context, themePid), 500L);
                 }
             } else if (compilingProcess) {
-                Log.d(SUBSTRATUM_LOG,
+                Substratum.log(SUBSTRATUM_LOG,
                         "Tried to restart activity but theme was compiling, delaying...");
                 shouldRestartActivity = true;
             }

app/src/main/java/projekt/substratum/MainActivity.java
@@ -58,6 +58,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
 import android.widget.TextView;
+import projekt.substratum.Substratum;
 import projekt.substratum.activities.launch.ShowcaseActivity;
 import projekt.substratum.common.Broadcasts;
 import projekt.substratum.common.Packages;
@@ -166,7 +167,7 @@ public class MainActivity extends AppCompatActivity implements
                     context,
                     overlay);
             if ((current_version <= OVERLAY_UPDATE_RANGE) && (current_version != 0)) {
-                Log.d("OverlayOutdatedCheck",
+                Substratum.log("OverlayOutdatedCheck",
                         "An overlay is returning " + current_version +
                                 " as Substratum's version, " +
                                 "this overlay is out of date, please uninstall and reinstall!");
@@ -294,7 +295,7 @@ public class MainActivity extends AppCompatActivity implements
         progressDialog.setCancelable(false);
 
         if (BuildConfig.DEBUG && !isSamsungDevice(context)) {
-            Log.d(SUBSTRATUM_LOG, "Substratum launched with debug mode signatures.");
+            Substratum.log(SUBSTRATUM_LOG, "Substratum launched with debug mode signatures.");
         }
         MainActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.main_activity);
         actionbarContent = binding.themeCount;
@@ -480,7 +481,7 @@ public class MainActivity extends AppCompatActivity implements
         if (TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS) >= 15L) {
             prefs.edit().putLong("previous_logchar_cleanup", currentDate.getTime()).apply();
             new ClearLogs(this).execute();
-            Log.d(SUBSTRATUM_LOG, "LogChar reports were wiped from the storage");
+            Substratum.log(SUBSTRATUM_LOG, "LogChar reports were wiped from the storage");
         }
     }
 
@@ -785,7 +786,7 @@ public class MainActivity extends AppCompatActivity implements
                         if (!deleted) Log.e(References.SUBSTRATUM_LOG,
                                 "Unable to delete directory");
                     } else {
-                        Log.d(References.SUBSTRATUM_LOG, "Deleting old cache dir: " + directory);
+                        Substratum.log(References.SUBSTRATUM_LOG, "Deleting old cache dir: " + directory);
                     }
                     if (!directory.exists()) {
                         boolean made = directory.mkdirs();
@@ -793,7 +794,7 @@ public class MainActivity extends AppCompatActivity implements
                                 "Unable to create directory");
                     } else {
                         References.injectRescueArchives(context);
-                        Log.d(References.SUBSTRATUM_LOG, "Successfully made dir: " + directory);
+                        Substratum.log(References.SUBSTRATUM_LOG, "Successfully made dir: " + directory);
                     }
                     File cacheDirectory = new File(getCacheDir(),
                             SUBSTRATUM_BUILDER_CACHE);
@@ -809,7 +810,7 @@ public class MainActivity extends AppCompatActivity implements
                                 .getAbsolutePath() +
                                 SUBSTRATUM_BUILDER_CACHE + file.getName());
                     }
-                    Log.d(SUBSTRATUM_BUILDER, "The cache has been flushed!");
+                    Substratum.log(SUBSTRATUM_BUILDER, "The cache has been flushed!");
                     References.injectRescueArchives(context);
                 } else {
                     // permission was not granted, show closing dialog

app/src/main/java/projekt/substratum/activities/launch/SplashScreenActivity.java
@@ -171,7 +171,7 @@ public class SplashScreenActivity extends Activity {
                 if (!prefs.contains(dateFormat.format(new Date()))) {
                     Log.e(SUBSTRATUM_LOG, "Failed to withdraw blacklisted packages...");
                 } else {
-                    Log.d(SUBSTRATUM_LOG, "Successfully withdrew blacklisted packages!");
+                    Substratum.log(SUBSTRATUM_LOG, "Successfully withdrew blacklisted packages!");
                 }
 
                 if (isAndromedaDevice(context)) {
@@ -201,7 +201,7 @@ public class SplashScreenActivity extends Activity {
                                     .putString("andromeda_installer", context.getPackageManager()
                                             .getInstallerPackageName(ANDROMEDA_PACKAGE))
                                     .apply();
-                            Log.d(SUBSTRATUM_LOG, "Successfully approved andromeda fingerprint!");
+                            Substratum.log(SUBSTRATUM_LOG, "Successfully approved andromeda fingerprint!");
                         }
                     }
                 }
@@ -225,7 +225,7 @@ public class SplashScreenActivity extends Activity {
                     if (!prefs2.contains("sungstratum_exp_fp_" + sstVersion)) {
                         Log.e(SUBSTRATUM_LOG, "Failed to withdraw sungstratum fingerprint...");
                     } else {
-                        Log.d(SUBSTRATUM_LOG, "Successfully approved sungstratum fingerprint!");
+                        Substratum.log(SUBSTRATUM_LOG, "Successfully approved sungstratum fingerprint!");
                     }
 
                     keyRetrieval = new KeyRetrieval();

app/src/main/java/projekt/substratum/common/Broadcasts.java
@@ -25,6 +25,7 @@ import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+import projekt.substratum.Substratum;
 import projekt.substratum.services.crash.AppCrashReceiver;
 import projekt.substratum.services.packages.OverlayFound;
 import projekt.substratum.services.packages.OverlayUpdater;
@@ -61,7 +62,7 @@ public enum Broadcasts {
     static void sendLocalizedKeyMessage(Context context,
                                         byte[] encryptionKey,
                                         byte[] ivEncryptKey) {
-        Log.d("KeyRetrieval",
+        Substratum.log("KeyRetrieval",
                 "The system has completed the handshake for keys retrieval " +
                         "and is now passing it to the activity...");
         Intent intent = new Intent(KEY_RETRIEVAL);
@@ -76,7 +77,7 @@ public enum Broadcasts {
      * @param context Context
      */
     public static void sendKillMessage(Context context) {
-        Log.d("SubstratumKiller",
+        Substratum.log("SubstratumKiller",
                 "A crucial action has been conducted by the user and " +
                         "Substratum is now shutting down!");
         Intent intent = new Intent(MAIN_ACTIVITY_RECEIVER);
@@ -89,7 +90,7 @@ public enum Broadcasts {
      * @param context Context
      */
     public static void sendRefreshMessage(Context context) {
-        Log.d("ThemeFragmentRefresher",
+        Substratum.log("ThemeFragmentRefresher",
                 "A theme has been modified, sending update signal to refresh the list!");
         Intent intent = new Intent(THEME_FRAGMENT_REFRESH);
         LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
@@ -101,7 +102,7 @@ public enum Broadcasts {
      * @param context Context
      */
     public static void sendOverlayRefreshMessage(Context context) {
-        Log.d("OverlayRefresher",
+        Substratum.log("OverlayRefresher",
                 "A theme has been modified, sending update signal to refresh the list!");
         Intent intent = new Intent(OVERLAY_REFRESH);
         LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
@@ -115,7 +116,7 @@ public enum Broadcasts {
      */
     public static void sendActivityFinisherMessage(Context context,
                                                    String packageName) {
-        Log.d("ThemeInstaller",
+        Substratum.log("ThemeInstaller",
                 "A theme has been installed, sending update signal to app for further processing!");
         Intent intent = new Intent(ACTIVITY_FINISHER);
         intent.putExtra(Internal.THEME_PID, packageName);
@@ -166,7 +167,7 @@ public enum Broadcasts {
             context.getApplicationContext().registerReceiver(
                     new PackageModificationDetector(), intentPackageFullyRemoved);
 
-            Log.d(SUBSTRATUM_LOG,
+            Substratum.log(SUBSTRATUM_LOG,
                     "Successfully registered broadcast receivers for Substratum functionality!");
         } catch (Exception e) {
             Log.e(SUBSTRATUM_LOG,
@@ -208,7 +209,7 @@ public enum Broadcasts {
             context.getApplicationContext().registerReceiver(
                     new KeyRetriever(), intentGetKeys);
 
-            Log.d(SUBSTRATUM_LOG, "Successfully registered key retrieval receiver!");
+            Substratum.log(SUBSTRATUM_LOG, "Successfully registered key retrieval receiver!");
         } catch (Exception e) {
             Log.e(SUBSTRATUM_LOG, "Failed to register key retrieval receiver...");
         }

app/src/main/java/projekt/substratum/common/Packages.java
@@ -722,7 +722,7 @@ public enum Packages {
                             packageName
                     };
                     returnMap.put(appInfo.metaData.getString(metadataName), data);
-                    Log.d(PACKAGE_TAG, "Loaded Substratum Theme: [" + packageName + ']');
+                    Substratum.log(PACKAGE_TAG, "Loaded Substratum Theme: [" + packageName + ']');
                     if (ENABLE_PACKAGE_LOGGING)
                         PackageAnalytics.logPackageInfo(context, packageName);
                 } else {

app/src/main/java/projekt/substratum/common/References.java
@@ -461,7 +461,7 @@ public enum References {
                 if (shortcutManager != null) {
                     shortcutManager.setDynamicShortcuts(Collections.singletonList(shortcut));
                 }
-                Log.d(SUBSTRATUM_LOG, "Successfully added dynamic app shortcut!");
+                Substratum.log(SUBSTRATUM_LOG, "Successfully added dynamic app shortcut!");
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -479,7 +479,7 @@ public enum References {
             if (shortcutManager != null) {
                 shortcutManager.removeAllDynamicShortcuts();
             }
-            Log.d(SUBSTRATUM_LOG, "Successfully removed all dynamic app shortcuts!");
+            Substratum.log(SUBSTRATUM_LOG, "Successfully removed all dynamic app shortcuts!");
         }
     }
 
@@ -741,7 +741,7 @@ public enum References {
         if (prefs.contains(date)) {
             Set<String> pref = prefs.getStringSet(date, new HashSet<>());
             if (checkPackageRegex(context, pref.toArray(new String[pref.size()]))) {
-                Log.d("PatcherDatabase",
+                Substratum.log("PatcherDatabase",
                         "The database has triggered a primary level blacklist package.");
                 uncertified = true;
                 return true;
@@ -805,7 +805,7 @@ public enum References {
             File logcharFolder = new File(Environment.getExternalStorageDirectory() +
                     File.separator + "substratum" + File.separator + "LogCharReports");
             if (!logcharFolder.exists() && logcharFolder.mkdirs()) {
-                Log.d("LogChar Utility", "Created LogChar directory!");
+                Substratum.log("LogChar Utility", "Created LogChar directory!");
             }
 
             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(

app/src/main/java/projekt/substratum/common/Resources.java
@@ -21,11 +21,12 @@ package projekt.substratum.common;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Build;
-import android.util.Log;
 
 import java.util.Arrays;
 
 import dalvik.system.DexClassLoader;
+
+import projekt.substratum.Substratum;
 
 import static projekt.substratum.common.References.INTERFACER_PACKAGE;
 import static projekt.substratum.common.References.SUBSTRATUM_LOG;
@@ -181,7 +182,7 @@ public enum Resources {
     @SuppressWarnings("JavaReflectionMemberAccess")
     public static boolean isFontsSupported(Context context) {
         if (checkSubstratumService(context)) {
-            Log.d(SUBSTRATUM_LOG, "This system fully supports font hotswapping.");
+            Substratum.log(SUBSTRATUM_LOG, "This system fully supports font hotswapping.");
             return true;
         } else if (checkOreo() && !checkSubstratumService(context)) {
             return false;
@@ -191,7 +192,7 @@ public enum Resources {
             cls.getDeclaredMethod("getSystemFontDirLocation");
             cls.getDeclaredMethod("getThemeFontConfigLocation");
             cls.getDeclaredMethod("getThemeFontDirLocation");
-            Log.d(SUBSTRATUM_LOG, "This system fully supports font hotswapping.");
+            Substratum.log(SUBSTRATUM_LOG, "This system fully supports font hotswapping.");
             return true;
         } catch (final Exception ignored) {
             return false;
@@ -212,7 +213,7 @@ public enum Resources {
     // This method checks whether custom shutdown animation is supported by the system
     public static boolean isShutdownAnimationSupported(Context context) {
         if (checkSubstratumService(context)) {
-            Log.d(SUBSTRATUM_LOG, "This system fully supports theme shutdown animation.");
+            Substratum.log(SUBSTRATUM_LOG, "This system fully supports theme shutdown animation.");
             return true;
         }
         try {
@@ -223,7 +224,7 @@ public enum Resources {
             cls.getDeclaredMethod("themeShutdownAnimationExists");
             cls.getDeclaredMethod("startShutdownAnimation");
             cls.getDeclaredMethod("stopShutdownAnimation");
-            Log.d(SUBSTRATUM_LOG, "This system fully supports theme shutdown animation.");
+            Substratum.log(SUBSTRATUM_LOG, "This system fully supports theme shutdown animation.");
             return true;
         } catch (Exception ignored) {
         }

commit d34ec4c3544e79a6b2687ef53e98a7a68f1553d3

Substratum: Underp log
I do weird things sometimes.

Fixes: 3f66c887895a ("treewide: Restrict debug logging to debug builds")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/Substratum.java
@@ -209,7 +209,7 @@ public class Substratum extends Application {
     }
 
     public static void log(final String TAG, final String message) {
-        if (BuildConfig.DEBUG)
+        if (!BuildConfig.DEBUG)
             return;
         Log.d(TAG, message);
     }
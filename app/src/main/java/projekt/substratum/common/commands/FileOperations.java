substratum/substratum

commit da90cb17d1cbfe39ad164219ce963c0f207e65b1

FileOperations: Directly use rooted operation when copying to system
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/common/commands/FileOperations.java
@@ -267,9 +267,16 @@ public enum FileOperations {
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY)
     private static void copy(final String source, final String destination) {
+        final File out = new File(destination);
+
+        if (destination.startsWith("/system")) {
+            Root.runCommand("cp -f " + source + ' ' + destination);
+            Substratum.log(COPY_LOG, "Operation " + (out.exists() ? "succeeded" : "failed"));
+            return;
+        }
+
         Substratum.log(COPY_LOG,
                 "Using rootless operation to copy " + source + " to " + destination);
-        final File out = new File(destination);
         try {
             final File in = new File(source);
             FileUtils.copyFile(in, out);

commit 41023b04fa3c1e59222b3bfb2a309e4a08eb92fa

FileOperations: Use root early for move operation as well
Fixes: da90cb17d1cb ("FileOperations: Directly use rooted operation when copying to system")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/common/commands/FileOperations.java
@@ -486,9 +486,16 @@ public enum FileOperations {
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY)
     private static void move(final String source, final String destination) {
-        Substratum.log(MOVE_LOG, "Using rootless operation to move " + source + " to " +
-                destination);
         final File out = new File(destination);
+
+        if (destination.startsWith("/system")) {
+            Root.runCommand("cp -f " + source + ' ' + destination);
+            Substratum.log(COPY_LOG, "Operation " + (out.exists() ? "succeeded" : "failed"));
+            return;
+        }
+
+        Substratum.log(COPY_LOG,
+                "Using rootless operation to copy " + source + " to " + destination);
         try {
             final File in = new File(source);
             if (in.isFile()) {
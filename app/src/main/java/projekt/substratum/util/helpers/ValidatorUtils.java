f4f5fa38ca520a7835d67180e42162c2e6c53e9c
Harsh Shandilya <harsh@prjkt.io>
2018-08-12 10:39:29 +0530

    treewide: Normalise copyrights and use SPDX identifiers
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- app/src/main/java/projekt/substratum/util/helpers/ValidatorUtils.java
+++ app/src/main/java/projekt/substratum/util/helpers/ValidatorUtils.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2016-2018 Projekt Substratum
+ * This file is part of Substratum.
+ *
+ * SPDX-License-Identifier: GPL-3.0-Or-Later
+ */
+
 package projekt.substratum.util.helpers;
 
 import android.app.Dialog;

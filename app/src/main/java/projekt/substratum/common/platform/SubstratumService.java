substratum/substratum

commit 462f15a558e8c4115846834fda0ec893ae1663f5

Update Pie methods for sysserv

app/src/main/java/projekt/substratum/common/platform/SubstratumService.java
@@ -26,6 +26,7 @@ public enum SubstratumService {
             "uninstallOverlay",
             "switchOverlay",
             "setPriority",
+            "setEnabled",
             "restartSystemUI",
             "copy",
             "move",
@@ -62,6 +63,15 @@ public enum SubstratumService {
         } catch (RemoteException e) {
             Log.e(TAG, "There was an exception when trying to get all overlay", e);
             return null;
+        }
+    }
+
+    public static boolean setEnabled(String packageName, boolean enable, int userId) {
+        try {
+            return service.setEnabled(packageName, enable, userId);
+        } catch (RemoteException e) {
+            Log.e(TAG, "There was an error enabling overlays", e);
+            return false;
         }
     }
 

app/src/main/java/projekt/substratum/common/Systems.java
@@ -216,7 +216,7 @@ public enum Systems {
                 } else if (sonyCheck == null || sonyCheck.length() == 0) {
                     Boolean isOMSRunning = isServiceRunning(IOverlayManager.class,
                             context.getApplicationContext());
-                    if (isOMSRunning || IS_OREO) {
+                    if (isOMSRunning || IS_OREO || IS_PIE) {
                         Substratum.log(SUBSTRATUM_LOG,
                                 "This device fully supports the Overlay Manager Service...");
                         foundOms = true;

app/src/main/java/projekt/substratum/common/platform/ThemeManager.java
@@ -379,7 +379,7 @@ public enum ThemeManager {
                         for (Map.Entry<String, List<OverlayInfo>> stringListEntry :
                                 allOverlays.entrySet()) {
                             for (OverlayInfo oi : stringListEntry.getValue()) {
-                                if (oi.isApproved()) {
+                                if (oi.isEnabled()) {
                                     list.add(oi.packageName);
                                 }
                             }
@@ -727,7 +727,7 @@ public enum ThemeManager {
             shouldRestartUi = shouldRestartUI(context, temp);
         }
 
-        if (Systems.IS_PIE) {
+        if ((Systems.IS_PIE) && checkSubstratumService(context)) {
             FileOperations.mountRW();
             for (String overlay : overlays) {
                 FileOperations.bruteforceDelete(P_DIR + '_' + overlay + ".apk");

app/src/main/java/projekt/substratum/util/compilers/SubstratumBuilder.java
@@ -609,7 +609,7 @@ public class SubstratumBuilder {
             }
         } catch (IOException ioe) {
             if (Systems.checkOMS(context) || Systems.isNewSamsungDeviceAndromeda(context)) {
-                Substratum.log(SUBSTRATUM_BUILDER, "An Android Oreo specific error message has been " +
+                Substratum.log(SUBSTRATUM_BUILDER, "An Android Oreo/Pie specific error message has been " +
                         "detected and has been whitelisted to continue moving forward " +
                         "with overlay compilation.");
                 return !hasErroredOut;

commit 0ddd665b4d1051bb3664ea7638f6181fccbf0552

SubstratumService: Break out Pie specific method from API check
Allows Oreo ROMs to pass API check again.

Fixes: 462f15a558e8 ("Update Pie methods for sysserv")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/common/platform/SubstratumService.java
@@ -11,10 +11,12 @@ import android.content.substratum.ISubstratumService;
 import android.os.Process;
 import android.os.RemoteException;
 import android.util.Log;
+import projekt.substratum.common.Systems;
 import projekt.substratum.platform.SubstratumServiceBridge;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -38,6 +40,7 @@ public enum SubstratumService {
             "applyShutdownAnimation",
             "getAllOverlays"
     };
+    private static final ArrayList<String> expectedMethods = new ArrayList<>(Arrays.asList(EXPECTED_METHODS));
     private static final int uid = Process.myUid() / 100000;
     private static final ISubstratumService service = SubstratumServiceBridge.get();
 
@@ -206,8 +209,10 @@ public enum SubstratumService {
 
     public static boolean checkApi() {
         try {
-            Method[] methods = ISubstratumService.class.getMethods();
-            for (String expectedMethod : EXPECTED_METHODS) {
+            final Method[] methods = ISubstratumService.class.getMethods();
+            if (Systems.IS_PIE)
+                expectedMethods.add("setEnabled");
+            for (String expectedMethod : expectedMethods) {
                 boolean methodFound = false;
                 for (Method method : methods) {
                     if (expectedMethod.equals(method.getName())) {
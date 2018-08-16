substratum/substratum

commit 0ddd665b4d1051bb3664ea7638f6181fccbf0552

SubstratumService: Break out Pie specific method from API check
Allows Oreo ROMs to pass API check again.

Fixes: 462f15a558e8 ("Update Pie methods for sysserv")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/common/platform/SubstratumService.java
@@ -11,10 +11,12 @@ import android.content.substratum.ISubstratumService;
 import android.os.Process;
 import android.os.RemoteException;
 import android.util.Log;
+import projekt.substratum.common.Systems;
 import projekt.substratum.platform.SubstratumServiceBridge;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -38,6 +40,7 @@ public enum SubstratumService {
             "applyShutdownAnimation",
             "getAllOverlays"
     };
+    private static final ArrayList<String> expectedMethods = new ArrayList<>(Arrays.asList(EXPECTED_METHODS));
     private static final int uid = Process.myUid() / 100000;
     private static final ISubstratumService service = SubstratumServiceBridge.get();
 
@@ -206,8 +209,10 @@ public enum SubstratumService {
 
     public static boolean checkApi() {
         try {
-            Method[] methods = ISubstratumService.class.getMethods();
-            for (String expectedMethod : EXPECTED_METHODS) {
+            final Method[] methods = ISubstratumService.class.getMethods();
+            if (Systems.IS_PIE)
+                expectedMethods.add("setEnabled");
+            for (String expectedMethod : expectedMethods) {
                 boolean methodFound = false;
                 for (Method method : methods) {
                     if (expectedMethod.equals(method.getName())) {

commit 6a96d5a572752ad7b11e3cd6b212658393a554a9

SubstratumService: Remove Pie specific method from default methods list
https://youtu.be/nQB4nAjZIdE

Fixes: 0ddd665b4d10 ("SubstratumService: Break out Pie specific method from API check")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/common/platform/SubstratumService.java
@@ -28,7 +28,6 @@ public enum SubstratumService {
             "uninstallOverlay",
             "switchOverlay",
             "setPriority",
-            "setEnabled",
             "restartSystemUI",
             "copy",
             "move",
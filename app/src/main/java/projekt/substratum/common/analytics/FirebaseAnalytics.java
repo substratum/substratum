substratum/substratum

commit 314d48408a4cc66fe43406011854026f606113fb

FirebaseAnalytics: Reduce connection timeout to five seconds
Grab fails quicker to determine if a VPN is needed.

Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/common/analytics/FirebaseAnalytics.java
@@ -47,7 +47,7 @@ public class FirebaseAnalytics {
             HttpsURLConnection connection = (HttpsURLConnection) new URL(
                     "https://console.firebase.google.com/").openConnection();
             connection.setRequestMethod("HEAD");
-            connection.setConnectTimeout(1000);
+            connection.setConnectTimeout(500);
             return connection.getResponseCode() == HttpsURLConnection.HTTP_OK;
         } catch (Exception ignored) {
         }

commit e26267b109f682836013773ba19a18668aa221a1

FirebaseAnalytics: Actually change connection timeout to five seconds
for (i in 1..1000)
    System.out.println("$i. A second has 1000ms\n")

Fixes: 314d48408a4c ("FirebaseAnalytics: Reduce connection timeout to five seconds")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/common/analytics/FirebaseAnalytics.java
@@ -47,7 +47,7 @@ public class FirebaseAnalytics {
             HttpsURLConnection connection = (HttpsURLConnection) new URL(
                     "https://console.firebase.google.com/").openConnection();
             connection.setRequestMethod("HEAD");
-            connection.setConnectTimeout(500);
+            connection.setConnectTimeout(5000);
             return connection.getResponseCode() == HttpsURLConnection.HTTP_OK;
         } catch (Exception ignored) {
         }
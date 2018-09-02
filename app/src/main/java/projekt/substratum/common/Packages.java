substratum/substratum

commit 2713be9bae1c327918c1cd436dc4275664aa7b6d

Cleanup uses of deprecated PackageInfo#versionCode
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/common/Packages.java
@@ -366,7 +366,7 @@ public class Packages {
                                         String packageName) {
         try {
             PackageInfo pInfo = context.getPackageManager().getPackageInfo(packageName, 0);
-            return pInfo.versionCode;
+            return (int) pInfo.getLongVersionCode();
         } catch (Exception ignored) {
         }
         return 0;

app/src/main/java/projekt/substratum/common/Systems.java
@@ -66,8 +66,8 @@ public class Systems {
     public static final boolean IS_NOUGAT = Build.VERSION.SDK_INT == Build.VERSION_CODES.N ||
             Build.VERSION.SDK_INT == Build.VERSION_CODES.N_MR1;
 
-    static Boolean checkPackageSupported;
-    static SharedPreferences prefs = Substratum.getPreferences();
+    private static Boolean checkPackageSupported;
+    private static SharedPreferences prefs = Substratum.getPreferences();
     /**
      * Cached boolean to find if system
      * passes all cases of Samsung checks
@@ -213,7 +213,7 @@ public class Systems {
                 if (checkThemeInterfacer(context) || checkSubstratumService(context)) {
                     foundOms = true;
                 } else if (sonyCheck == null || sonyCheck.length() == 0) {
-                    Boolean isOMSRunning = isServiceRunning(IOverlayManager.class,
+                    boolean isOMSRunning = isServiceRunning(IOverlayManager.class,
                             context.getApplicationContext());
                     if (isOMSRunning || IS_OREO || IS_PIE) {
                         Substratum.log(SUBSTRATUM_LOG,
@@ -269,7 +269,7 @@ public class Systems {
      */
     public static void setAndCheckSubstratumService() {
         StringBuilder check = References.runShellCommand("cmd -l");
-        Boolean present = check != null && check.toString().contains("substratum");
+        boolean present = check != null && check.toString().contains("substratum");
         prefs.edit().putBoolean("substratum_service_present", false).apply();
         if (present) {
             prefs.edit().putBoolean("substratum_service_present", true).apply();
@@ -346,7 +346,7 @@ public class Systems {
         boolean isEnabled = Packages.isAvailablePackage(context, References
                 .INTERFACER_PACKAGE);
         PackageInfo packageInfo = getThemeInterfacerPackage(context);
-        return (packageInfo != null) && (packageInfo.versionCode >= 60) && isEnabled;
+        return (packageInfo != null) && (packageInfo.getLongVersionCode() >= 60) && isEnabled;
     }
 
     /**
@@ -617,8 +617,8 @@ public class Systems {
      * @param stringArray List of packages to check
      * @return True if blacklisted packages found
      */
-    public static Boolean checkPackageRegex(Context context,
-                                            String[] stringArray) {
+    static Boolean checkPackageRegex(Context context,
+                                     String[] stringArray) {
         if (stringArray.length == 0) return true;
         final PackageManager pm = context.getPackageManager();
         List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
@@ -658,7 +658,7 @@ public class Systems {
 
             Map<String, String> listOfRoms =
                     ReadSupportedROMsFile.read(context.getCacheDir() + "/" + fileName);
-            Boolean supported = false;
+            boolean supported = false;
 
             // First check if it is a valid prop
             for (Object o : listOfRoms.entrySet()) {

app/src/main/java/projekt/substratum/fragments/SettingsFragment.java
@@ -545,8 +545,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                         boolean isEnabled = (boolean) newValue;
                         autoDisableTargetOverlays.setVisible(!isEnabled);
                         prefs.edit().putBoolean("auto_disable_target_overlays", !isEnabled).apply();
-                        prefs.edit().putBoolean("sungstromeda_mode",
-                                isEnabled).apply();
+                        prefs.edit().putBoolean("sungstromeda_mode", isEnabled).apply();
                         sungstromedaMode.setChecked((Boolean) newValue);
                         new Handler().postDelayed(() ->
                                 Substratum.restartSubstratum(context), 1000L);
@@ -595,7 +594,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 try {
                     PackageInfo info =
                             context.getPackageManager().getPackageInfo(ANDROMEDA_PACKAGE, 0);
-                    aboutAndromeda.setSummary(info.versionName + " (" + info.versionCode + ')');
+                    aboutAndromeda.setSummary(info.versionName + " (" + info.getLongVersionCode() + ')');
                 } catch (Exception ignored) {
                 }
                 aboutAndromeda.setOnPreferenceClickListener(preference -> {
@@ -635,7 +634,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                     PackageInfo pInfo = Systems.getThemeInterfacerPackage(context);
                     if (pInfo != null) {
                         aboutInterfacer.setSummary(
-                                pInfo.versionName + " (" + pInfo.versionCode + ')');
+                                pInfo.versionName + " (" + pInfo.getLongVersionCode() + ')');
                     }
                 } catch (Exception e) {
                     // Suppress exception
@@ -746,7 +745,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 try {
                     PackageInfo info =
                             context.getPackageManager().getPackageInfo(ANDROMEDA_PACKAGE, 0);
-                    aboutAndromeda.setSummary(info.versionName + " (" + info.versionCode + ')');
+                    aboutAndromeda.setSummary(info.versionName + " (" + info.getLongVersionCode() + ')');
                 } catch (Exception ignored) {
                 }
                 aboutAndromeda.setOnPreferenceClickListener(preference -> {
@@ -763,7 +762,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 aboutSamsung.setIcon(Packages.getAppIcon(context, SST_ADDON_PACKAGE));
                 try {
                     PackageInfo info = context.getPackageManager().getPackageInfo(SST_ADDON_PACKAGE, 0);
-                    aboutSamsung.setSummary(info.versionName + " (" + info.versionCode + ')');
+                    aboutSamsung.setSummary(info.versionName + " (" + info.getLongVersionCode() + ')');
                 } catch (Exception ignored) {
                 }
                 showDangerousSamsung.setChecked(

commit b01a2a88f5c3a7ddc3df367722a9b1ec03e7713b

treewide: Restore use of deprecated API
Availability of the method appears to be inconsistent across
platform versions.

Fixes: 2713be9bae1c ("Cleanup uses of deprecated PackageInfo#versionCode")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/common/Packages.java
@@ -366,7 +366,7 @@ public class Packages {
                                         String packageName) {
         try {
             PackageInfo pInfo = context.getPackageManager().getPackageInfo(packageName, 0);
-            return (int) pInfo.getLongVersionCode();
+            return pInfo.versionCode;
         } catch (Exception ignored) {
         }
         return 0;

app/src/main/java/projekt/substratum/common/Systems.java
@@ -346,7 +346,7 @@ public class Systems {
         boolean isEnabled = Packages.isAvailablePackage(context, References
                 .INTERFACER_PACKAGE);
         PackageInfo packageInfo = getThemeInterfacerPackage(context);
-        return (packageInfo != null) && (packageInfo.getLongVersionCode() >= 60) && isEnabled;
+        return (packageInfo != null) && (packageInfo.versionCode >= 60) && isEnabled;
     }
 
     /**

app/src/main/java/projekt/substratum/fragments/SettingsFragment.java
@@ -594,7 +594,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 try {
                     PackageInfo info =
                             context.getPackageManager().getPackageInfo(ANDROMEDA_PACKAGE, 0);
-                    aboutAndromeda.setSummary(info.versionName + " (" + info.getLongVersionCode() + ')');
+                    aboutAndromeda.setSummary(info.versionName + " (" + info.versionCode + ')');
                 } catch (Exception ignored) {
                 }
                 aboutAndromeda.setOnPreferenceClickListener(preference -> {
@@ -634,7 +634,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                     PackageInfo pInfo = Systems.getThemeInterfacerPackage(context);
                     if (pInfo != null) {
                         aboutInterfacer.setSummary(
-                                pInfo.versionName + " (" + pInfo.getLongVersionCode() + ')');
+                                pInfo.versionName + " (" + pInfo.versionCode + ')');
                     }
                 } catch (Exception e) {
                     // Suppress exception
@@ -745,7 +745,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 try {
                     PackageInfo info =
                             context.getPackageManager().getPackageInfo(ANDROMEDA_PACKAGE, 0);
-                    aboutAndromeda.setSummary(info.versionName + " (" + info.getLongVersionCode() + ')');
+                    aboutAndromeda.setSummary(info.versionName + " (" + info.versionCode + ')');
                 } catch (Exception ignored) {
                 }
                 aboutAndromeda.setOnPreferenceClickListener(preference -> {
@@ -762,7 +762,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 aboutSamsung.setIcon(Packages.getAppIcon(context, SST_ADDON_PACKAGE));
                 try {
                     PackageInfo info = context.getPackageManager().getPackageInfo(SST_ADDON_PACKAGE, 0);
-                    aboutSamsung.setSummary(info.versionName + " (" + info.getLongVersionCode() + ')');
+                    aboutSamsung.setSummary(info.versionName + " (" + info.versionCode + ')');
                 } catch (Exception ignored) {
                 }
                 showDangerousSamsung.setChecked(
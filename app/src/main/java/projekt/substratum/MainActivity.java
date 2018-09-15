substratum/substratum

commit 1fdaea25fd42c2774e511386f00e9cc2dd90563e

Migrate to AndroidX
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/MainActivity.java
@@ -20,26 +20,11 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
-import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.service.quicksettings.Tile;
-import android.support.annotation.NonNull;
-import android.support.annotation.RestrictTo;
-import android.support.design.internal.BottomNavigationItemView;
-import android.support.design.internal.BottomNavigationMenuView;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MenuItem;
@@ -47,6 +32,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.RestrictTo;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
 import projekt.substratum.activities.launch.ShowcaseActivity;
 import projekt.substratum.common.Broadcasts;
 import projekt.substratum.common.Packages;
@@ -75,7 +75,6 @@ import projekt.substratum.util.helpers.Root;
 
 import java.io.File;
 import java.lang.ref.WeakReference;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -343,26 +342,8 @@ public class MainActivity extends AppCompatActivity implements
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         getSupportActionBar().setDisplayShowHomeEnabled(false);
 
+        bottomBar.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
         menuView = (BottomNavigationMenuView) bottomBar.getChildAt(0);
-
-        try {
-            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
-            shiftingMode.setAccessible(true);
-            shiftingMode.setBoolean(menuView, false);
-            shiftingMode.setAccessible(false);
-            for (int i = 0; i < menuView.getChildCount(); i++) {
-                BottomNavigationItemView item =
-                        (BottomNavigationItemView) menuView.getChildAt(i);
-                //noinspection RestrictedApi
-                item.setShiftingMode(false);
-                // set once again checked value, so view will be updated
-                //noinspection RestrictedApi
-                item.setChecked(item.getItemData().isChecked());
-            }
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            // suppress exception
-        }
-
         if (Systems.checkOMS(context) && !isSamsung(context)) {
             menuView.findViewById(R.id.tab_priorities).setVisibility(View.VISIBLE);
             menuView.findViewById(R.id.tab_profiles).setVisibility(View.VISIBLE);

app/src/main/java/projekt/substratum/activities/launch/SplashScreenActivity.java
@@ -13,7 +13,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.databinding.DataBindingUtil;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -21,6 +20,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import androidx.databinding.DataBindingUtil;
 import com.jaredrummler.android.widget.AnimatedSvgView;
 import projekt.substratum.MainActivity;
 import projekt.substratum.R;

commit e717db7ac69c89365ce631ee0ca85ae820090963

Repair offline mode
Switching to Andromeda/Sungstromeda offline mode
ideally should hide themes, profiles and priorities
options from the menu since they're non-functional.
This was not happening since the AndroidX migration
because a dedicated API for this was introduced which
we were not using.

Fixes: 1fdaea25fd42 ("Migrate to AndroidX")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/MainActivity.java
@@ -1112,12 +1112,9 @@ public class MainActivity extends AppCompatActivity implements
                         titleView.setVisibility(View.GONE);
                     } else if (Systems.isAndromedaDevice(context) &&
                             !AndromedaService.checkServerActivity()) {
-                        TextView andromedaTitle = activity.progressDialog.findViewById(
-                                R.id.andromeda_title);
-                        Button andromedaOfflineButton =
-                                activity.progressDialog.findViewById(R.id.andromeda_offline_button);
-                        TextView andromedaDebugText =
-                                activity.progressDialog.findViewById(R.id.andromeda_debug_text);
+                        TextView andromedaTitle = activity.progressDialog.findViewById(R.id.andromeda_title);
+                        Button andromedaOfflineButton = activity.progressDialog.findViewById(R.id.andromeda_offline_button);
+                        TextView andromedaDebugText = activity.progressDialog.findViewById(R.id.andromeda_debug_text);
                         andromedaTitle.setVisibility(View.VISIBLE);
                         if (!checkAndromeda(context)) {
                             andromedaTitle.setText(R.string.andromeda_no_firebase);
@@ -1133,8 +1130,7 @@ public class MainActivity extends AppCompatActivity implements
                                             getAppVersionCode(context, ANDROMEDA_PACKAGE) > 19
                                                     ? "activities.InfoActivity" : "InfoActivity"));
                             andromedaOfflineButton.setVisibility(View.VISIBLE);
-                            andromedaOfflineButton.setOnClickListener(v ->
-                                    activity.progressDialog.cancel());
+                            andromedaOfflineButton.setOnClickListener(v -> activity.progressDialog.cancel());
                             if (BuildConfig.DEBUG) {
                                 andromedaDebugText.setVisibility(View.VISIBLE);
                             }
@@ -1143,14 +1139,10 @@ public class MainActivity extends AppCompatActivity implements
                         textView.setVisibility(View.GONE);
                         titleView.setVisibility(View.GONE);
                         instanceBasedAndromedaFailure = true;
-                        activity.menuView.
-                                findViewById(R.id.tab_themes).setVisibility(View.GONE);
-                        activity.menuView.
-                                findViewById(R.id.tab_priorities).setVisibility(View.GONE);
-                        activity.menuView.
-                                findViewById(R.id.tab_profiles).setVisibility(View.GONE);
-                        activity.bottomBar.
-                                setSelectedItemId(R.id.tab_overlay_manager);
+                        activity.bottomBar.getMenu().removeItem(R.id.tab_themes);
+                        activity.bottomBar.getMenu().removeItem(R.id.tab_priorities);
+                        activity.bottomBar.getMenu().removeItem(R.id.tab_profiles);
+                        activity.bottomBar.setSelectedItemId(R.id.tab_overlay_manager);
                     } else if (Systems.IS_OREO &&
                             !Packages.isPackageInstalled(context, ANDROMEDA_PACKAGE)) {
                         TextView andromedaTitle = activity.progressDialog.findViewById(
@@ -1159,8 +1151,7 @@ public class MainActivity extends AppCompatActivity implements
                         Button andromedaButton = activity.progressDialog.findViewById(
                                 R.id.andromeda_button);
                         andromedaButton.setVisibility(View.VISIBLE);
-                        andromedaButton.setOnClickListener(view ->
-                                launchActivityUrl(context, R.string.andromeda_url));
+                        andromedaButton.setOnClickListener(view -> launchActivityUrl(context, R.string.andromeda_url));
                         textView.setVisibility(View.GONE);
                         titleView.setVisibility(View.GONE);
                     }

app/src/main/java/projekt/substratum/activities/launch/SplashScreenActivity.java
@@ -166,8 +166,7 @@ public class SplashScreenActivity extends Activity {
                 if (isAndromedaDevice(context)) {
                     int andromedaVer = Packages.getAppVersionCode(context, ANDROMEDA_PACKAGE);
                     FirebaseAnalytics.withdrawAndromedaFingerprint(context, andromedaVer);
-                    SharedPreferences prefs2 =
-                            context.getSharedPreferences("substratum_state", Context.MODE_PRIVATE);
+                    SharedPreferences prefs2 = context.getSharedPreferences("substratum_state", Context.MODE_PRIVATE);
                     timeoutCount = 0;
                     while (!prefs2.contains("andromeda_exp_fp_" + andromedaVer) &&
                             (timeoutCount < 100)) {
@@ -199,8 +198,7 @@ public class SplashScreenActivity extends Activity {
                         Packages.isPackageInstalled(context, SST_ADDON_PACKAGE)) {
                     int sstVersion = Packages.getAppVersionCode(context, SST_ADDON_PACKAGE);
                     FirebaseAnalytics.withdrawSungstratumFingerprint(context, sstVersion);
-                    SharedPreferences prefs2 =
-                            context.getSharedPreferences("substratum_state", Context.MODE_PRIVATE);
+                    SharedPreferences prefs2 = context.getSharedPreferences("substratum_state", Context.MODE_PRIVATE);
                     timeoutCount = 0;
                     while (!prefs2.contains("sungstratum_exp_fp_" + sstVersion) && (timeoutCount <
                             100)) {
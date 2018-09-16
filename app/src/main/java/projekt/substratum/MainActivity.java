substratum/substratum

commit e717db7ac69c89365ce631ee0ca85ae820090963

Repair offline mode
Switching to Andromeda/Sungstromeda offline mode
ideally should hide themes, profiles and priorities
options from the menu since they're non-functional.
This was not happening since the AndroidX migration
because a dedicated API for this was introduced which
we were not using.

Fixes: 1fdaea25fd42 ("Migrate to AndroidX")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/MainActivity.java
@@ -1112,12 +1112,9 @@ public class MainActivity extends AppCompatActivity implements
                         titleView.setVisibility(View.GONE);
                     } else if (Systems.isAndromedaDevice(context) &&
                             !AndromedaService.checkServerActivity()) {
-                        TextView andromedaTitle = activity.progressDialog.findViewById(
-                                R.id.andromeda_title);
-                        Button andromedaOfflineButton =
-                                activity.progressDialog.findViewById(R.id.andromeda_offline_button);
-                        TextView andromedaDebugText =
-                                activity.progressDialog.findViewById(R.id.andromeda_debug_text);
+                        TextView andromedaTitle = activity.progressDialog.findViewById(R.id.andromeda_title);
+                        Button andromedaOfflineButton = activity.progressDialog.findViewById(R.id.andromeda_offline_button);
+                        TextView andromedaDebugText = activity.progressDialog.findViewById(R.id.andromeda_debug_text);
                         andromedaTitle.setVisibility(View.VISIBLE);
                         if (!checkAndromeda(context)) {
                             andromedaTitle.setText(R.string.andromeda_no_firebase);
@@ -1133,8 +1130,7 @@ public class MainActivity extends AppCompatActivity implements
                                             getAppVersionCode(context, ANDROMEDA_PACKAGE) > 19
                                                     ? "activities.InfoActivity" : "InfoActivity"));
                             andromedaOfflineButton.setVisibility(View.VISIBLE);
-                            andromedaOfflineButton.setOnClickListener(v ->
-                                    activity.progressDialog.cancel());
+                            andromedaOfflineButton.setOnClickListener(v -> activity.progressDialog.cancel());
                             if (BuildConfig.DEBUG) {
                                 andromedaDebugText.setVisibility(View.VISIBLE);
                             }
@@ -1143,14 +1139,10 @@ public class MainActivity extends AppCompatActivity implements
                         textView.setVisibility(View.GONE);
                         titleView.setVisibility(View.GONE);
                         instanceBasedAndromedaFailure = true;
-                        activity.menuView.
-                                findViewById(R.id.tab_themes).setVisibility(View.GONE);
-                        activity.menuView.
-                                findViewById(R.id.tab_priorities).setVisibility(View.GONE);
-                        activity.menuView.
-                                findViewById(R.id.tab_profiles).setVisibility(View.GONE);
-                        activity.bottomBar.
-                                setSelectedItemId(R.id.tab_overlay_manager);
+                        activity.bottomBar.getMenu().removeItem(R.id.tab_themes);
+                        activity.bottomBar.getMenu().removeItem(R.id.tab_priorities);
+                        activity.bottomBar.getMenu().removeItem(R.id.tab_profiles);
+                        activity.bottomBar.setSelectedItemId(R.id.tab_overlay_manager);
                     } else if (Systems.IS_OREO &&
                             !Packages.isPackageInstalled(context, ANDROMEDA_PACKAGE)) {
                         TextView andromedaTitle = activity.progressDialog.findViewById(
@@ -1159,8 +1151,7 @@ public class MainActivity extends AppCompatActivity implements
                         Button andromedaButton = activity.progressDialog.findViewById(
                                 R.id.andromeda_button);
                         andromedaButton.setVisibility(View.VISIBLE);
-                        andromedaButton.setOnClickListener(view ->
-                                launchActivityUrl(context, R.string.andromeda_url));
+                        andromedaButton.setOnClickListener(view -> launchActivityUrl(context, R.string.andromeda_url));
                         textView.setVisibility(View.GONE);
                         titleView.setVisibility(View.GONE);
                     }

app/src/main/java/projekt/substratum/activities/launch/SplashScreenActivity.java
@@ -166,8 +166,7 @@ public class SplashScreenActivity extends Activity {
                 if (isAndromedaDevice(context)) {
                     int andromedaVer = Packages.getAppVersionCode(context, ANDROMEDA_PACKAGE);
                     FirebaseAnalytics.withdrawAndromedaFingerprint(context, andromedaVer);
-                    SharedPreferences prefs2 =
-                            context.getSharedPreferences("substratum_state", Context.MODE_PRIVATE);
+                    SharedPreferences prefs2 = context.getSharedPreferences("substratum_state", Context.MODE_PRIVATE);
                     timeoutCount = 0;
                     while (!prefs2.contains("andromeda_exp_fp_" + andromedaVer) &&
                             (timeoutCount < 100)) {
@@ -199,8 +198,7 @@ public class SplashScreenActivity extends Activity {
                         Packages.isPackageInstalled(context, SST_ADDON_PACKAGE)) {
                     int sstVersion = Packages.getAppVersionCode(context, SST_ADDON_PACKAGE);
                     FirebaseAnalytics.withdrawSungstratumFingerprint(context, sstVersion);
-                    SharedPreferences prefs2 =
-                            context.getSharedPreferences("substratum_state", Context.MODE_PRIVATE);
+                    SharedPreferences prefs2 = context.getSharedPreferences("substratum_state", Context.MODE_PRIVATE);
                     timeoutCount = 0;
                     while (!prefs2.contains("sungstratum_exp_fp_" + sstVersion) && (timeoutCount <
                             100)) {

commit 2f81379f7a1b7480a6345d3703674ca2a42354d9

MainActivity: Fix bottombar for sungstromeda mode
Fixes: e717db7ac69c ("Repair offline mode")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/projekt/substratum/MainActivity.java
@@ -135,7 +135,7 @@ public class MainActivity extends AppCompatActivity implements
     private ActionBar supportActionBar;
     private int permissionCheck = PackageManager.PERMISSION_DENIED;
     private Dialog progressDialog;
-    private SharedPreferences prefs = Substratum.getPreferences();
+    private static final SharedPreferences prefs = Substratum.getPreferences();
     private LocalBroadcastManager localBroadcastManager;
     private KillReceiver killReceiver;
     private AndromedaReceiver andromedaReceiver;
@@ -1111,7 +1111,7 @@ public class MainActivity extends AppCompatActivity implements
                         textView.setVisibility(View.GONE);
                         titleView.setVisibility(View.GONE);
                     } else if (Systems.isAndromedaDevice(context) &&
-                            !AndromedaService.checkServerActivity()) {
+                            (!AndromedaService.checkServerActivity() || prefs.getBoolean("sungstromeda_mode", false))) {
                         TextView andromedaTitle = activity.progressDialog.findViewById(R.id.andromeda_title);
                         Button andromedaOfflineButton = activity.progressDialog.findViewById(R.id.andromeda_offline_button);
                         TextView andromedaDebugText = activity.progressDialog.findViewById(R.id.andromeda_debug_text);
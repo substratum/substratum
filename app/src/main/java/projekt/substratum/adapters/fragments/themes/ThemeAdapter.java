b65bb05ca122dd9922b343e09550c1e9f99208fa
Harsh Shandilya <harsh@prjkt.io>
2018-10-31 14:19:41 +0530

    ThemeAdapter: Don't hide the entire bottom sheet for system themes
    The only operation which cannot be performed on a theme that's also
    a system app is uninstall, so make the changes as needed.
    
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- app/src/main/java/projekt/substratum/adapters/fragments/themes/ThemeAdapter.java
+++ app/src/main/java/projekt/substratum/adapters/fragments/themes/ThemeAdapter.java
@@ -63,87 +63,67 @@ public class ThemeAdapter extends RecyclerView.Adapter<ThemeAdapter.ViewHolder> 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder,
                                  int pos) {
-        ThemeItem themeItem = this.information.get(pos);
+        final ThemeItem themeItem = this.information.get(pos);
         this.context = themeItem.getContext();
-        ThemeEntryCardBinding viewHolderBinding = viewHolder.getBinding();
-        SharedPreferences prefs = Substratum.getPreferences();
+        final ThemeEntryCardBinding viewHolderBinding = viewHolder.getBinding();
+        final SharedPreferences prefs = Substratum.getPreferences();
 
         viewHolderBinding.themeCard.setOnClickListener(
                 v -> Theming.launchTheme(this.context, themeItem.getThemePackage()));
 
         viewHolderBinding.themeCard.setOnLongClickListener(view -> {
             // Vibrate the device alerting the user they are about to do something dangerous!
-            if (Packages.isUserApp(this.context, themeItem.getThemePackage())) {
-                Vibrator v = (Vibrator) this.context.getSystemService(Context.VIBRATOR_SERVICE);
-                if (v != null) {
-                    v.vibrate(30L);
-                }
+            Vibrator v = (Vibrator) this.context.getSystemService(Context.VIBRATOR_SERVICE);
+            if (v != null) {
+                v.vibrate(30L);
+            }
 
-                // About the theme
-                SheetDialog sheetDialog = new SheetDialog(this.context);
-                View sheetView =
-                        View.inflate(this.context,
-                                R.layout.theme_entry_long_press_sheet_dialog, null);
-                ThemeEntryLongPressSheetDialogBinding sheetDialogBinding = DataBindingUtil.bind(sheetView);
+            // About the theme
+            SheetDialog sheetDialog = new SheetDialog(this.context);
+            View sheetView =
+                    View.inflate(this.context,
+                            R.layout.theme_entry_long_press_sheet_dialog, null);
+            ThemeEntryLongPressSheetDialogBinding sheetDialogBinding = DataBindingUtil.bind(sheetView);
 
-                assert sheetDialogBinding != null;
-                TextView aboutText = sheetDialogBinding.aboutText;
-                aboutText.setText(themeItem.getThemeName());
-                aboutText.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
-                sheetDialogBinding.moreText.setText(String.format("%s (%s)\n%s",
-                        Packages.getAppVersion(this.context, themeItem.getThemePackage()),
-                        Packages.getAppVersionCode(this.context, themeItem.getThemePackage()),
-                        Packages.getPackageTemplateVersion(this.context,
-                                themeItem.getThemePackage())));
+            assert sheetDialogBinding != null;
+            TextView aboutText = sheetDialogBinding.aboutText;
+            aboutText.setText(themeItem.getThemeName());
+            aboutText.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+            sheetDialogBinding.moreText.setText(String.format("%s (%s)\n%s",
+                    Packages.getAppVersion(this.context, themeItem.getThemePackage()),
+                    Packages.getAppVersionCode(this.context, themeItem.getThemePackage()),
+                    Packages.getPackageTemplateVersion(this.context,
+                            themeItem.getThemePackage())));
 
-                sheetDialogBinding.icon
-                        .setImageDrawable(Packages.getAppIcon(this.context, themeItem
-                                .getThemePackage()));
+            sheetDialogBinding.icon
+                    .setImageDrawable(Packages.getAppIcon(this.context, themeItem
+                            .getThemePackage()));
 
-                // Favorite
-                LinearLayout favorite = sheetDialogBinding.favorite;
+            // Favorite
+            LinearLayout favorite = sheetDialogBinding.favorite;
 
-                Drawable favoriteImg = this.context.getDrawable(R.drawable.toolbar_favorite);
-                Drawable notFavoriteImg =
-                        this.context.getDrawable(R.drawable.toolbar_not_favorite);
-                TextView favoriteText = sheetView.findViewById(R.id.favorite_text);
-                if (prefs.getString("app_shortcut_theme", "").equals(themeItem.getThemePackage())) {
-                    assert favoriteImg != null;
-                    favoriteImg.setBounds(0, 0, 60, 60);
-                    favoriteText.setCompoundDrawables(favoriteImg, null, null, null);
-                    favoriteText.setText(this.context.getString(R.string.menu_unfavorite));
-                } else {
-                    assert notFavoriteImg != null;
-                    notFavoriteImg.setBounds(0, 0, 60, 60);
-                    favoriteText.setCompoundDrawables(notFavoriteImg, null, null, null);
-                    favoriteText.setText(this.context.getString(R.string.menu_favorite));
-                }
+            Drawable favoriteImg = this.context.getDrawable(R.drawable.toolbar_favorite);
+            Drawable notFavoriteImg =
+                    this.context.getDrawable(R.drawable.toolbar_not_favorite);
+            TextView favoriteText = sheetView.findViewById(R.id.favorite_text);
+            if (prefs.getString("app_shortcut_theme", "").equals(themeItem.getThemePackage())) {
+                assert favoriteImg != null;
+                favoriteImg.setBounds(0, 0, 60, 60);
+                favoriteText.setCompoundDrawables(favoriteImg, null, null, null);
+                favoriteText.setText(this.context.getString(R.string.menu_unfavorite));
+            } else {
+                assert notFavoriteImg != null;
+                notFavoriteImg.setBounds(0, 0, 60, 60);
+                favoriteText.setCompoundDrawables(notFavoriteImg, null, null, null);
+                favoriteText.setText(this.context.getString(R.string.menu_favorite));
+            }
 
-                favorite.setOnClickListener(view1 -> {
-                    if (prefs.contains("app_shortcut_theme")) {
-                        if (!prefs.getString("app_shortcut_theme", "").equals(
-                                themeItem.getThemePackage())) {
-                            prefs.edit().remove("app_shortcut_theme").apply();
-                            References.clearShortcut(this.context);
-                            prefs.edit().putString("app_shortcut_theme",
-                                    themeItem.getThemePackage()).apply();
-                            References.createShortcut(
-                                    this.context,
-                                    themeItem.getThemePackage(),
-                                    themeItem.getThemeName());
-                            assert favoriteImg != null;
-                            favoriteImg.setBounds(0, 0, 60, 60);
-                            favoriteText.setCompoundDrawables(favoriteImg, null, null, null);
-                            favoriteText.setText(this.context.getString(R.string.menu_unfavorite));
-                        } else {
-                            prefs.edit().remove("app_shortcut_theme").apply();
-                            References.clearShortcut(this.context);
-                            assert notFavoriteImg != null;
-                            notFavoriteImg.setBounds(0, 0, 60, 60);
-                            favoriteText.setCompoundDrawables(notFavoriteImg, null, null, null);
-                            favoriteText.setText(this.context.getString(R.string.menu_favorite));
-                        }
-                    } else {
+            favorite.setOnClickListener(view1 -> {
+                if (prefs.contains("app_shortcut_theme")) {
+                    if (!prefs.getString("app_shortcut_theme", "").equals(
+                            themeItem.getThemePackage())) {
+                        prefs.edit().remove("app_shortcut_theme").apply();
+                        References.clearShortcut(this.context);
                         prefs.edit().putString("app_shortcut_theme",
                                 themeItem.getThemePackage()).apply();
                         References.createShortcut(
@@ -154,59 +134,81 @@ public class ThemeAdapter extends RecyclerView.Adapter<ThemeAdapter.ViewHolder> 
                         favoriteImg.setBounds(0, 0, 60, 60);
                         favoriteText.setCompoundDrawables(favoriteImg, null, null, null);
                         favoriteText.setText(this.context.getString(R.string.menu_unfavorite));
+                    } else {
+                        prefs.edit().remove("app_shortcut_theme").apply();
+                        References.clearShortcut(this.context);
+                        assert notFavoriteImg != null;
+                        notFavoriteImg.setBounds(0, 0, 60, 60);
+                        favoriteText.setCompoundDrawables(notFavoriteImg, null, null, null);
+                        favoriteText.setText(this.context.getString(R.string.menu_favorite));
                     }
-                });
-
-                // Rate Theme
-                LinearLayout rate = sheetDialogBinding.rate;
-                String installer =
-                        Packages.getInstallerId(this.context, themeItem.getThemePackage());
-                if ((installer != null) && installer.equals(PLAY_STORE_PACKAGE_NAME)) {
-                    rate.setVisibility(View.VISIBLE);
                 } else {
-                    rate.setVisibility(View.GONE);
+                    prefs.edit().putString("app_shortcut_theme",
+                            themeItem.getThemePackage()).apply();
+                    References.createShortcut(
+                            this.context,
+                            themeItem.getThemePackage(),
+                            themeItem.getThemeName());
+                    assert favoriteImg != null;
+                    favoriteImg.setBounds(0, 0, 60, 60);
+                    favoriteText.setCompoundDrawables(favoriteImg, null, null, null);
+                    favoriteText.setText(this.context.getString(R.string.menu_unfavorite));
                 }
-                rate.setOnClickListener(view12 -> {
-                    try {
-                        String playURL = PLAY_URL_PREFIX + themeItem.getThemePackage();
-                        Intent i = new Intent(Intent.ACTION_VIEW);
-                        i.setData(Uri.parse(playURL));
-                        this.context.startActivity(i);
-                    } catch (ActivityNotFoundException activityNotFoundException) {
-                        Toast.makeText(
-                                this.context,
-                                this.context.getString(R.string.activity_missing_toast),
-                                Toast.LENGTH_SHORT).show();
-                    }
-                    sheetDialog.dismiss();
-                });
+            });
 
-                // Shortcut
-                LinearLayout shortcut = sheetDialogBinding.shortcut;
-                shortcut.setOnClickListener(view12 -> {
-                    References.createLauncherIcon(this.context,
-                            themeItem.getThemePackage(), themeItem.getThemeName());
-                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-                        Toast.makeText(
-                                this.context,
-                                this.context.getString(R.string.launcher_shortcut_toast),
-                                Toast.LENGTH_SHORT).show();
-                    }
-                    sheetDialog.dismiss();
-                });
+            // Rate Theme
+            LinearLayout rate = sheetDialogBinding.rate;
+            String installer =
+                    Packages.getInstallerId(this.context, themeItem.getThemePackage());
+            if ((installer != null) && installer.equals(PLAY_STORE_PACKAGE_NAME)) {
+                rate.setVisibility(View.VISIBLE);
+            } else {
+                rate.setVisibility(View.GONE);
+            }
+            rate.setOnClickListener(view12 -> {
+                try {
+                    String playURL = PLAY_URL_PREFIX + themeItem.getThemePackage();
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(playURL));
+                    this.context.startActivity(i);
+                } catch (ActivityNotFoundException activityNotFoundException) {
+                    Toast.makeText(
+                            this.context,
+                            this.context.getString(R.string.activity_missing_toast),
+                            Toast.LENGTH_SHORT).show();
+                }
+                sheetDialog.dismiss();
+            });
 
-                // Uninstalling
-                LinearLayout uninstall = sheetDialogBinding.uninstall;
+            // Shortcut
+            LinearLayout shortcut = sheetDialogBinding.shortcut;
+            shortcut.setOnClickListener(view12 -> {
+                References.createLauncherIcon(this.context,
+                        themeItem.getThemePackage(), themeItem.getThemeName());
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+                    Toast.makeText(
+                            this.context,
+                            this.context.getString(R.string.launcher_shortcut_toast),
+                            Toast.LENGTH_SHORT).show();
+                }
+                sheetDialog.dismiss();
+            });
+
+            // Uninstalling
+            LinearLayout uninstall = sheetDialogBinding.uninstall;
+            if (!Packages.isUserApp(this.context, themeItem.getThemePackage())) {
+                uninstall.setVisibility(View.GONE);
+            } else {
                 uninstall.setOnClickListener(view2 -> {
                     Uri packageURI = Uri.parse("package:" + themeItem.getThemePackage());
                     Intent uninstallIntent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE, packageURI);
                     this.context.startActivity(uninstallIntent);
                     sheetDialog.dismiss();
                 });
-
-                sheetDialog.setContentView(sheetView);
-                sheetDialog.show();
             }
+
+            sheetDialog.setContentView(sheetView);
+            sheetDialog.show();
             return false;
         });
         viewHolderBinding.setThemeItem(themeItem);

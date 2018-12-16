f53ee23e75fb63e8345e5527c32bf8108a9a9350
Harsh Shandilya <harsh@prjkt.io>
2018-12-16 21:48:49 +0530

    build: Don't use rootProject to open keystore
    skip-ci
    
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
             create(buildTypeRelease) {
                 keyAlias = keystoreProperties["keyAlias"].toString()
                 keyPassword = keystoreProperties["keyPassword"].toString()
-                storeFile = rootProject.file(keystoreProperties["storeFile"].toString())
+                storeFile = file(keystoreProperties["storeFile"].toString())
                 storePassword = keystoreProperties["storePassword"].toString()
             }
         }

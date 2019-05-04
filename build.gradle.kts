be549e9736743d86f12b88d96c0ad6e46399eb36
Harsh Shandilya <harsh@prjkt.io>
2019-05-04 17:12:24 +0530

    build: Allow alpha revisions in dependencyUpdates task
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,20 +30,6 @@ tasks {
         outputFormatter = "json"
         outputDir = "build/dependencyUpdates"
         reportfileName = "report"
-        resolutionStrategy {
-            componentSelection {
-                all {
-                    val rejected = listOf("alpha", "beta", "rc", "cr", "m", "preview")
-                      .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-]*") }
-                      .any { it.matches(candidate.version) }
-                    if (rejected) {
-                        reject("Release candidate")
-                    } else if (candidate.group.equals("commons-io") && candidate.version.toFloat() >= 2.6F) {
-                        reject("commons-io v2.6+ cause runtime crashes due to missing java.nio.File class in Android 7")
-                    }
-                }
-            }
-        }
     }
 
     named<Wrapper>("wrapper") {
